4ef385d1a63f44012346c71ece6d8d6fe48a66e1
Steve Haar <info@stevehaar.com>
2022-04-01 13:00:18 -0500

    chore: remove run-func build dependency

--- scripts/get-changed-files.js
+++ scripts/get-changed-files.js
@@ -4,18 +4,34 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
+/*
+  This script will execute a command and pass a filtered list of all new and modified files as the last argument to the command.
+  The first argument is a minimatch filter to apply to the list of new and modified files
+  The following arguments together make up the command
+  For example, `node get-changed-files.js "**.*.css" prettier --write`, will run prettier in write mode for all new and modified css files.
+  There are easier ways to do this, such as using Unix "xargs", but we need to make sure this works on Windows too.
+*/
+
 const childProcess = require('child_process');
 const minimatch = require('minimatch');
 
-function main(filter) {
-  const files = getChanges()
+const filter = process.argv[2];
+const command = process.argv.slice(3);
+const files = getFilteredChanges(filter);
+
+runCommandOnChangedFiles(command, files);
+
+function runCommandOnChangedFiles(command, files) {
+  if (files.length) {
+    childProcess.spawn('npx', ['-q', ...command, ...files], { stdio: 'inherit' });
+  }
+}
+
+function getFilteredChanges(filter) {
+  return getChanges()
     .filter(file => file.status !== 'D')
     .filter(file => !filter || minimatch(file.name, filter))
     .map(file => file.name);
-
-  if (files.length) {
-    childProcess.spawn('npx', ['-q', ...[...arguments].splice(1), ...files], { stdio: 'inherit' });
-  }
 }
 
 function getChanges() {
@@ -33,5 +49,3 @@ function getFile(entry) {
   const name = status === 'RM' ? data[3] : data[1];
   return { status, name };
 }
-
-module.exports = { main };
