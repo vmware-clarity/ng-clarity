7155271d1ce340725d0ed8880b99870838b173d3
Ivan Donchev <idonchev@vmware.com>
2022-05-11 18:56:05 +0300

    fix: fix multiple overlay nesting scenarios
    closes #111
    Signed-off-by: Ivan Donchev <idonchev@vmware.com>

---
+++ projects/demo/src/app/z-index/z-index.demo.module.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { ClarityModule } from '@clr/angular';
+import { ZIndexCustomFilter } from './custom-filter.demo';
+import { ZIndexVariousContent } from './various-content/various-content';
+import { ZIndexDemo } from './z-index.demo';
+import { ROUTING } from './z-index.routing';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, ClarityModule, ROUTING],
+  declarations: [ZIndexDemo, ZIndexVariousContent, ZIndexCustomFilter],
+  exports: [ZIndexDemo],
+})
+export class ZIndexDemoModule {}
