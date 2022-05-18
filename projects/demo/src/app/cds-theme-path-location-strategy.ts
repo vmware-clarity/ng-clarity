59f4e1fa2148d341b594b85de523486e67e44ceb
Kevin Buhmann <kbuhmann@vmware.com>
2022-05-18 13:53:32 -0500

    chore: add cds theme to query string in demo app
    This enables persisting the theme across page reloads and sharing links
    with a specific theme enabled.

---
+++ projects/demo/src/app/cds-theme-path-location-strategy.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { APP_BASE_HREF, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
+import { Inject, Injectable, Optional, Provider } from '@angular/core';
+import { UrlSerializer } from '@angular/router';
+import { cdsThemeAttribute, getCdsThemeFromDom } from './cds-theme-select.component';
+
+/**
+ * This path location strategy adds the current theme to the query string so that
+ * the selected theme persists across refreshes and link sharing.
+ */
+@Injectable()
+export class CdsThemePathLocationStrategy extends PathLocationStrategy {
+  constructor(
+    platformLocation: PlatformLocation,
+    @Optional() @Inject(APP_BASE_HREF) baseHref: string,
+    private readonly urlSerializer: UrlSerializer
+  ) {
+    super(platformLocation, baseHref);
+  }
+
+  override prepareExternalUrl(internal: string): string {
+    const url = super.prepareExternalUrl(internal);
+    const urlTree = this.urlSerializer.parse(url);
+
+    const currentTheme = getCdsThemeFromDom();
+
+    if (currentTheme) {
+      urlTree.queryParams = { ...urlTree.queryParams, [cdsThemeAttribute]: currentTheme };
+    }
+
+    return urlTree.toString();
+  }
+}
+
+export const cdsThemePathLocationStrategyProvider: Provider = {
+  provide: LocationStrategy,
+  useClass: CdsThemePathLocationStrategy,
+};
--- projects/demo/src/app/cds-theme-select.component.ts
+++ projects/demo/src/app/cds-theme-select.component.ts
@@ -4,16 +4,17 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Params, Router } from '@angular/router';
 
-const cdsThemeAttribute = 'cds-theme';
+export const cdsThemeAttribute = 'cds-theme';
 
 @Component({
   selector: 'app-cds-theme-select',
   template: `
     <clr-select-container>
       <label>Cds Theme</label>
-      <select clrSelect (change)="applyTheme($event.target.value)">
+      <select clrSelect [value]="theme" (change)="applyTheme($event.target.value)">
         <option value="">None</option>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
@@ -21,14 +22,36 @@ const cdsThemeAttribute = 'cds-theme';
     </clr-select-container>
   `,
 })
-export class CdsThemeSelectComponent implements OnDestroy {
+export class CdsThemeSelectComponent implements OnInit, OnDestroy {
+  theme = '';
+
+  constructor(private readonly router: Router) {}
+
+  ngOnInit() {
+    this.applyTheme(getCdsThemeFromQueryString());
+  }
+
   ngOnDestroy() {
     this.applyTheme(null);
   }
 
   applyTheme(theme: string) {
+    this.theme = theme || '';
+
     setThemeInDom(theme);
+    this.setThemeInQueryString(theme);
   }
+
+  private setThemeInQueryString(theme: string) {
+    this.router.navigate([], {
+      queryParams: { ...getQueryParams(), [cdsThemeAttribute]: theme || null },
+      replaceUrl: true,
+    });
+  }
+}
+
+export function getCdsThemeFromDom() {
+  return document.body.getAttribute(cdsThemeAttribute);
 }
 
 function setThemeInDom(theme: string) {
@@ -37,4 +60,21 @@ function setThemeInDom(theme: string) {
   } else {
     document.body.removeAttribute(cdsThemeAttribute);
   }
+}
+
+function getCdsThemeFromQueryString() {
+  return getQueryParams()[cdsThemeAttribute];
+}
+
+function getQueryParams() {
+  // This code uses `window.location.search` instead of the Angular router because of this bug: https://github.com/angular/angular/issues/12157.
+  const urlSearchParams = new URLSearchParams(window.location.search);
+
+  const queryParams: Params = {};
+
+  urlSearchParams.forEach((value, key) => {
+    queryParams[key] = value;
+  });
+
+  return queryParams;
 }
