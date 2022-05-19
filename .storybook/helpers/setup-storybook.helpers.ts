cdf9bc4170f5e84f58a8915b0673edb5a0bf0104
Steve Haar <info@stevehaar.com>
2022-05-19 15:24:44 -0500

    chore: add storybook
    Co-Authored-By: Kevin Buhmann <kbuhmann@vmware.com>

---
+++ .storybook/helpers/setup-storybook.helpers.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { CommonModule } from '@angular/common';
+import { Type } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Parameters } from '@storybook/addons';
+import { moduleMetadata, storiesOf, Story } from '@storybook/angular';
+import { NgLetModule } from 'ng-let';
+
+export function setupStorybook(
+  ngModules: Type<any> | Type<any>[],
+  defaultStory: Story,
+  defaultParameters: Parameters,
+  variants: Parameters[]
+) {
+  const storyApi = storiesOf(defaultParameters.title, module)
+    .addParameters(defaultParameters)
+    .addDecorator(
+      moduleMetadata({
+        imports: [
+          CommonModule,
+          BrowserAnimationsModule,
+          ReactiveFormsModule,
+          NgLetModule,
+          ...(Array.isArray(ngModules) ? ngModules : [ngModules]),
+        ],
+      })
+    );
+
+  storyApi.add('Default', defaultStory, defaultParameters);
+
+  storyApi.add('Variants', variants.length ? combineStories(defaultStory, variants) : defaultStory, {
+    a11y: { disable: true },
+    actions: { disable: true },
+    controls: { disable: true },
+    previewTabs: { 'storybook/docs/panel': { hidden: true } },
+  });
+}
+
+function combineStories(defaultStory: Story, variants: Parameters[]): Story {
+  return (args, context) => ({
+    template: variants
+      .map(variant => {
+        const containers = Object.entries(variant).map(([key, value]) => `<ng-container ${ngLet({ key, value })}>`);
+        const prefix = containers.join('');
+        const suffix = containers.map(() => '</ng-container>').join('');
+
+        const story = defaultStory({ ...args, ...variant }, context);
+        const template = story.template;
+
+        return `${prefix}${template}${suffix}`;
+      })
+      .join('<br />'),
+    props: { ...args },
+  });
+}
+
+function ngLet({ key, value }: { key: string; value: any }) {
+  return `*ngLet="${typeof value === 'string' ? `'${value}'` : JSON.stringify(value)}; let ${key}"`;
+}
---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+module.exports = {
+  stories: ['./stories/**/*.stories.{ts,mdx}'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+  framework: '@storybook/angular',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
