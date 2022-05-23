01b162051d145510886353eaab5589a31bea8baf
Steve Haar <info@stevehaar.com>
2022-05-23 15:24:11 -0500

    chore: fixup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,10 +6,47 @@
 
 import { setCompodocJson } from '@storybook/addon-docs/angular';
 import docs from './../documentation.json';
+import styles from './../dist/clr-ui/clr-ui.css';
 
-setCompodocJson(docs);
+const privateModifier = 121;
+
+addStyles(styles);
+addDocs(docs);
 
 export const parameters = {
   docs: { inlineStories: true },
   chromatic: { disableSnapshot: true },
 };
+
+function addStyles(styles) {
+  const style = document.createElement('style');
+  style.textContent = `${styles}`;
+  window.document.head.append(style);
+}
+
+function addDocs(docs) {
+  removeProperties(docs);
+  removePrivateMethods(docs);
+  removeAngularLifeCycleMethods(docs);
+  setCompodocJson(docs);
+}
+
+function removeProperties(docs) {
+  docs.components.forEach(component => {
+    delete component.propertiesClass;
+  });
+}
+
+function removePrivateMethods(docs) {
+  docs.components.forEach(component => {
+    component.methodsClass = component.methodsClass.filter(
+      method => method.modifierKind?.includes(privateModifier) !== true
+    );
+  });
+}
+
+function removeAngularLifeCycleMethods(docs) {
+  docs.components.forEach(component => {
+    component.methodsClass = component.methodsClass.filter(method => method.name.startsWith('ng') === false);
+  });
+}
