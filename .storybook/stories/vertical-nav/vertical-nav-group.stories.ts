2e096c8b0edfeb1f632afcd5bd6f6dc39fe8aba1
Kevin Buhmann <kbuhmann@vmware.com>
2022-05-24 14:34:29 -0500

    chore: add vertical nav stories

---
+++ .storybook/stories/vertical-nav/vertical-nav-group.stories.ts
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { bellIcon, calendarIcon, folderIcon, homeIcon, searchIcon, userIcon } from '@cds/core/icon';
+import { IconShapeTuple } from '@cds/core/icon/interfaces/icon.interfaces';
+import { ClrVerticalNavGroup, ClrVerticalNavModule } from '@clr/angular';
+import { action } from '@storybook/addon-actions';
+import { Parameters } from '@storybook/addons';
+import { Story } from '@storybook/angular';
+
+import { setupStorybook } from '../../helpers/setup-storybook.helpers';
+
+const navLinks: { iconShapeTuple: IconShapeTuple; text: string }[] = [
+  { iconShapeTuple: bellIcon, text: 'Notifications' },
+  { iconShapeTuple: homeIcon, text: 'Dashboard' },
+  { iconShapeTuple: searchIcon, text: 'Search' },
+  { iconShapeTuple: calendarIcon, text: 'Calendar' },
+  { iconShapeTuple: folderIcon, text: 'Files' },
+  { iconShapeTuple: userIcon, text: 'Profile' },
+];
+
+const defaultStory: Story = args => ({
+  template: `
+    <div class="main-container">
+      <clr-vertical-nav [clrVerticalNavCollapsible]="true">
+        <clr-vertical-nav-group
+          [clrVerticalNavGroupLabel]="clrVerticalNavGroupLabel"
+          [clrVerticalNavGroupExpanded]="clrVerticalNavGroupExpanded"
+          (clrVerticalNavGroupExpandedChange)="clrVerticalNavGroupExpandedChange($event)"
+        >
+          <cds-icon *ngIf="includeIcons" shape="bars" clrVerticalNavIcon></cds-icon>
+          Menu
+          <clr-vertical-nav-group-children>
+            <a *ngFor="let navLink of navLinks; let index = index"
+              clrVerticalNavLink
+              [ngClass]="{ 'active': index == activeIndex }"
+              href="javascript:void(0)"
+              (click)="activeIndex = index"
+            >
+              <cds-icon *ngIf="includeIcons" [attr.shape]="navLink.iconShapeTuple[0]" clrVerticalNavIcon></cds-icon>
+              {{navLink.text}}
+            </a>
+          </clr-vertical-nav-group-children>
+        </clr-vertical-nav-group>
+      </clr-vertical-nav>
+    </div>
+  `,
+  props: { ...args },
+});
+
+const defaultParameters: Parameters = {
+  title: 'Vertical Nav/Vertical Nav Group',
+  component: ClrVerticalNavGroup,
+  argTypes: {
+    // inputs
+    clrVerticalNavGroupLabel: { defaultValue: 'Toggle vertical navigation group', control: { type: 'text' } },
+    clrVerticalNavGroupExpanded: { defaultValue: false, control: { type: 'boolean' } },
+    // outputs
+    clrVerticalNavGroupExpandedChange: { control: { disable: true } },
+    // methods
+    collapseGroup: { control: { disable: true }, table: { disable: true } },
+    expandAnimationDone: { control: { disable: true }, table: { disable: true } },
+    expandGroup: { control: { disable: true }, table: { disable: true } },
+    toggleExpand: { control: { disable: true }, table: { disable: true } },
+    // story helpers
+    navLinks: { control: { disable: true }, table: { disable: true } },
+  },
+  args: {
+    // outputs
+    clrVerticalNavGroupExpandedChange: action('clrVerticalNavGroupExpandedChange'),
+    // story helpers
+    navLinks,
+    activeIndex: 0,
+    includeIcons: true,
+  },
+};
+
+const variants: Parameters[] = [
+  {
+    clrVerticalNavGroupExpanded: false,
+    includeIcons: true,
+  },
+  {
+    clrVerticalNavGroupExpanded: true,
+    includeIcons: true,
+  },
+  {
+    clrVerticalNavGroupExpanded: false,
+    includeIcons: false,
+  },
+  {
+    clrVerticalNavGroupExpanded: true,
+    includeIcons: false,
+  },
+];
+
+setupStorybook(ClrVerticalNavModule, defaultStory, defaultParameters, variants);
---
+++ .storybook/stories/vertical-nav/vertical-nav.stories.ts
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { bellIcon, calendarIcon, folderIcon, homeIcon, searchIcon, userIcon } from '@cds/core/icon';
+import { IconShapeTuple } from '@cds/core/icon/interfaces/icon.interfaces';
+import { ClrVerticalNav, ClrVerticalNavModule } from '@clr/angular';
+import { action } from '@storybook/addon-actions';
+import { Parameters } from '@storybook/addons';
+import { Story } from '@storybook/angular';
+
+import { setupStorybook } from '../../helpers/setup-storybook.helpers';
+
+const navLinks: { iconShapeTuple: IconShapeTuple; text: string }[] = [
+  { iconShapeTuple: bellIcon, text: 'Notifications' },
+  { iconShapeTuple: homeIcon, text: 'Dashboard' },
+  { iconShapeTuple: searchIcon, text: 'Search' },
+  { iconShapeTuple: calendarIcon, text: 'Calendar' },
+  { iconShapeTuple: folderIcon, text: 'Files' },
+  { iconShapeTuple: userIcon, text: 'Profile' },
+];
+
+const defaultStory: Story = args => ({
+  template: `
+    <div class="main-container">
+      <clr-vertical-nav
+        [clrVerticalNavCollapsible]="clrVerticalNavCollapsible"
+        [clrVerticalNavCollapsed]="clrVerticalNavCollapsed"
+        (clrVerticalNavCollapsedChange)="clrVerticalNavCollapsedChange($event)"
+      >
+        <a
+          *ngFor="let navLink of navLinks; let index = index"
+          clrVerticalNavLink
+          [ngClass]="{ 'active': index == activeIndex }"
+          href="javascript:void(0)"
+          (click)="activeIndex = index"
+        >
+          <cds-icon *ngIf="includeIcons" [attr.shape]="navLink.iconShapeTuple[0]" clrVerticalNavIcon></cds-icon>
+          {{navLink.text}}
+        </a>
+      </clr-vertical-nav>
+    </div>
+  `,
+  props: { ...args },
+});
+
+const defaultParameters: Parameters = {
+  title: 'Vertical Nav/Vertical Nav',
+  component: ClrVerticalNav,
+  argTypes: {
+    // inputs
+    clrVerticalNavCollapsible: { defaultValue: false, control: { type: 'boolean' } },
+    clrVerticalNavCollapsed: { defaultValue: false, control: { type: 'boolean' } },
+    // outputs
+    clrVerticalNavCollapsedChange: { control: { disable: true } },
+    // methods
+    toggleByButton: { control: { disable: true }, table: { disable: true } },
+    // story helpers
+    navLinks: { control: { disable: true }, table: { disable: true } },
+  },
+  args: {
+    // outputs
+    clrVerticalNavCollapsedChange: action('clrVerticalNavCollapsedChange'),
+    // story helpers
+    navLinks,
+    activeIndex: 0,
+    includeIcons: true,
+  },
+};
+
+const variants: Parameters[] = [
+  {
+    clrVerticalNavCollapsible: false,
+    clrVerticalNavCollapsed: false,
+    includeIcons: true,
+  },
+  {
+    clrVerticalNavCollapsible: false,
+    clrVerticalNavCollapsed: false,
+    includeIcons: false,
+  },
+  {
+    clrVerticalNavCollapsible: true,
+    clrVerticalNavCollapsed: false,
+    includeIcons: true,
+  },
+  {
+    clrVerticalNavCollapsible: true,
+    clrVerticalNavCollapsed: false,
+    includeIcons: false,
+  },
+  {
+    clrVerticalNavCollapsible: true,
+    clrVerticalNavCollapsed: true,
+    includeIcons: true,
+  },
+];
+
+setupStorybook(ClrVerticalNavModule, defaultStory, defaultParameters, variants);
