aefdc4c3e341e58a055ac6e53d29caf8acf8523c
Steve Haar <info@stevehaar.com>
2022-06-07 10:14:46 -0500

    chore: add radio stories

---
+++ .storybook/stories/radio/radio-container.stories.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { ClrRadioContainer, ClrRadioModule } from '@clr/angular';
+import { Parameters } from '@storybook/addons';
+import { Story } from '@storybook/angular';
+
+import { setupStorybook } from '../../helpers/setup-storybook.helpers';
+
+const defaultStory: Story = args => ({
+  template: ` 
+    <clr-radio-container [clrInline]="clrInline">
+      <label>{{label}}</label>
+      <clr-radio-wrapper *ngFor="let _ of createArray(optionCount); let i = index">
+        <input type="radio" clrRadio name="options" value="i + 1" />
+        <label>Option {{i + 1}}</label>
+      </clr-radio-wrapper>
+    </clr-radio-container>
+  `,
+  props: { ...args },
+});
+
+const defaultParameters: Parameters = {
+  title: 'Radio/Radio Container',
+  component: ClrRadioContainer,
+  argTypes: {
+    // inputs
+    clrInline: { defaultValue: false, control: { type: 'boolean' } },
+    // methods
+    addGrid: { control: { disabled: true }, table: { disable: true } },
+    controlClass: { control: { disabled: true }, table: { disable: true } },
+    // story helpers
+    createArray: { control: { disable: true }, table: { disable: true } },
+  },
+  args: {
+    // story helpers
+    label: 'Options',
+    createArray: n => new Array(n),
+    optionCount: 3,
+  },
+};
+
+const variants: Parameters[] = [
+  {
+    clrInline: false,
+  },
+  {
+    clrInline: true,
+  },
+];
+
+setupStorybook(ClrRadioModule, defaultStory, defaultParameters, variants);
