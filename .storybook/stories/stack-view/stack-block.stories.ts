047f4c2f12ef55610ca42f0842e1755d6efd9bb6
Kevin Buhmann <kbuhmann@vmware.com>
2022-06-23 13:08:38 -0500

    chore: add stack view stories
    I did not document inline editing as that feature appears to be
    experimental.

---
+++ .storybook/stories/stack-view/stack-block.stories.ts
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { ClrStackBlock, ClrStackViewModule } from '@clr/angular';
+import { action } from '@storybook/addon-actions';
+import { Parameters } from '@storybook/addons';
+import { Story } from '@storybook/angular';
+
+import { setupStorybook } from '../../helpers/setup-storybook.helpers';
+
+const defaultStory: Story = args => ({
+  template: `
+    <clr-stack-view>
+      <clr-stack-block
+        ${args.clrSbExpandable === undefined ? '' : '[clrSbExpandable]="clrSbExpandable"'}
+        [clrSbExpanded]="clrSbExpanded"
+        [clrSbNotifyChange]="clrSbNotifyChange"
+        [clrStackViewLevel]="clrStackViewLevel"
+        (clrSbExpandedChange)="clrSbExpandedChange($event)"
+      >
+        <clr-stack-label>{{ label }}</clr-stack-label>
+        <clr-stack-content>{{ content }}</clr-stack-content>
+        <clr-stack-block>
+          <clr-stack-label>{{ subLabel }}</clr-stack-label>
+          <clr-stack-content>{{ subContent }}</clr-stack-content>
+        </clr-stack-block>
+      </clr-stack-block>
+    </clr-stack-view>
+  `,
+  props: { ...args },
+});
+
+const defaultParameters: Parameters = {
+  title: 'Stack View/Stack Block',
+  component: ClrStackBlock,
+  argTypes: {
+    // inputs
+    clrSbNotifyChange: { control: { disable: true }, table: { disable: true } }, // experimental
+    clrStackViewPosinset: { control: { disable: true }, table: { disable: true } }, // deprecated
+    clrStackViewSetsize: { control: { disable: true }, table: { disable: true } }, // deprecated
+    // outputs
+    clrSbExpandedChange: { control: { disable: true }, table: { disable: true } },
+    // methods
+    addChild: { control: { disable: true }, table: { disable: true } },
+    getStackChildrenId: { control: { disable: true }, table: { disable: true } },
+    toggleExpand: { control: { disable: true }, table: { disable: true } },
+  },
+  args: {
+    // inputs
+    clrSbExpandable: undefined,
+    // outputs
+    clrSbExpandedChange: action('clrSbExpandedChange'),
+    // story helpers
+    label: 'Block',
+    content: 'Block content',
+    subLabel: 'Sub-block',
+    subContent: 'Sub-block content',
+  },
+};
+
+const variants: Parameters[] = [{ clrSbExpandable: false }, { clrSbExpanded: false }, { clrSbExpanded: true }];
+
+setupStorybook(ClrStackViewModule, defaultStory, defaultParameters, variants);
---
+++ .storybook/stories/stack-view/stack-view-with-lazy-loaded-blocks.stories.ts
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { ClrSpinnerModule, ClrStackView, ClrStackViewModule } from '@clr/angular';
+import { Parameters } from '@storybook/addons';
+import { Story } from '@storybook/angular';
+import { Observable, timer } from 'rxjs';
+import { mapTo, tap } from 'rxjs/operators';
+
+import { elements } from '../../helpers/elements.data';
+import { setupStorybook } from '../../helpers/setup-storybook.helpers';
+
+interface Block {
+  label: string;
+  content: string;
+}
+
+class ElementsBlockService {
+  loading = false;
+  blocks: Observable<Block[]>;
+
+  getBlocks() {
+    this.loading = true;
+
+    this.blocks = timer(1000).pipe(
+      mapTo(elements.map<Block>(element => ({ label: element.symbol, content: element.name }))),
+      tap(() => {
+        this.loading = false;
+      })
+    );
+  }
+}
+
+const defaultStory: Story = args => ({
+  template: `
+    <clr-stack-view>
+      <clr-stack-block [clrSbExpandable]="true" (clrSbExpandedChange)="elementsBlockService.getBlocks()">
+        <clr-stack-label>Elements</clr-stack-label>
+
+        <clr-stack-block *ngIf="elementsBlockService.loading">
+          <clr-spinner clrInline>Loading...</clr-spinner>
+        </clr-stack-block>
+
+        <clr-stack-block *ngFor="let block of elementsBlockService.blocks | async">
+          <clr-stack-label>{{ block.label }}</clr-stack-label>
+          <clr-stack-content>{{ block.content }}</clr-stack-content>
+        </clr-stack-block>
+      </clr-stack-block>
+    </clr-stack-view>
+  `,
+  props: { ...args },
+});
+
+const defaultParameters: Parameters = {
+  title: 'Stack View/Stack View with lazy-loaded blocks',
+  component: ClrStackView,
+  argTypes: {
+    // outputs
+    clrStackSave: { control: { disable: true }, table: { disable: true } }, // experimental
+    // story helpers
+    elementsBlockService: { control: { disable: true }, table: { disable: true } },
+  },
+  args: {
+    // story helpers
+    elementsBlockService: new ElementsBlockService(),
+  },
+};
+
+const variants: Parameters[] = [];
+
+setupStorybook([ClrStackViewModule, ClrSpinnerModule], defaultStory, defaultParameters, variants);
