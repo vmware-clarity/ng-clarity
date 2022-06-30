1ff6115ebf389dca9bfd0d04e3654ec715907fbb
Kevin Buhmann <kbuhmann@vmware.com>
2022-06-30 12:50:44 -0500

    chore: use path alias instead of symlink for `@clr/angular`

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,11 +4,20 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['./stories/**/*.stories.{ts,mdx}'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: '@storybook/angular',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal(config) {
+    config.resolve = config.resolve || {};
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(new TsconfigPathsPlugin({ configFile: './.storybook/tsconfig.json' }));
+
+    return config;
   },
 };
