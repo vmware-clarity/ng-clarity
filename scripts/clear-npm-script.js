508bcc3d5c8f1ed06a6f77fd9efd3d40ec513012
Kevin Buhmann <kbuhmann@vmware.com>
2022-07-06 13:48:14 -0500

    chore: build with each allowed major version of @cds/core

---
+++ scripts/clear-npm-script.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+/*
+ * This script will clear/disable the specified scripts in package.json.
+ * It is used to skip certain build steps in pr-build.yml in certain build configurations.
+ */
+
+const fs = require('fs');
+
+const [, , ...scriptNames] = process.argv;
+
+const packageManifestPath = 'package.json';
+const packageManifest = JSON.parse(fs.readFileSync(packageManifestPath, 'utf8'));
+
+for (const scriptName of scriptNames) {
+  packageManifest.scripts[scriptName] = '';
+}
+
+fs.writeFileSync(packageManifestPath, JSON.stringify(packageManifest, null, 2) + '\n');
