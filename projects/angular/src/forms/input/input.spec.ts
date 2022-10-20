2f7bd1799253a3a96068cc30030a2db99e5b11ee
Ivan Donchev <idonchev@vmware.com>
2022-10-20 13:54:18 +0300

    fix(forms): remove touched enforcement
    This was a bug fix for old issue with input type=number that is not reproducible anymore.
    It causes extra valueChanged events on blur after upgrade to Angular v14.
    
    closes #337
    
    Signed-off-by: Ivan Donchev <idonchev@vmware.com>

--- projects/angular/src/forms/input/input.spec.ts
+++ projects/angular/src/forms/input/input.spec.ts
@@ -5,8 +5,11 @@
  */
 
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup, FormsModule, NgControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
+import { ClrCommonFormsModule } from '../common';
 import { ControlStandaloneSpec, ReactiveSpec, TemplateDrivenSpec } from '../tests/control.spec';
 import { ClrInput } from './input';
 import { ClrInputContainer } from './input-container';
@@ -39,5 +42,46 @@ export default function (): void {
     ControlStandaloneSpec(StandaloneUseTest);
     TemplateDrivenSpec(ClrInputContainer, ClrInput, TemplateDrivenTest, 'clr-input');
     ReactiveSpec(ClrInputContainer, ClrInput, ReactiveTest, 'clr-input');
+    inputSpec('tempate-driven', ClrInputContainer, ClrInput, TemplateDrivenTest);
+    inputSpec('reactive', ClrInputContainer, ClrInput, ReactiveTest);
+  });
+}
+
+function inputSpec(description, testContainer, testControl, testComponent) {
+  describe('Input specific value change tests ' + description, () => {
+    let control, fixture;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [FormsModule, ClrCommonFormsModule, ReactiveFormsModule],
+        declarations: [testContainer, testControl, testComponent],
+      });
+      fixture = TestBed.createComponent(testComponent);
+      control = fixture.debugElement.query(By.directive(testControl));
+      fixture.detectChanges();
+    });
+
+    it('should handle valueChanges calls', () => {
+      // control must be both invalid and blurred to register the validity
+      let valueChanges = 0;
+      control.injector.get(NgControl).control.valueChanges.subscribe(() => {
+        valueChanges++;
+      });
+
+      // make sure blur alone does not trigger valueChanges
+      control.nativeElement.dispatchEvent(new Event('focus'));
+      control.nativeElement.dispatchEvent(new Event('blur'));
+      fixture.detectChanges();
+      expect(valueChanges).toBe(0);
+
+      // now make sure input change triggers valueChanges
+      control.nativeElement.dispatchEvent(new Event('focus'));
+      control.nativeElement.value = 'abc';
+
+      control.nativeElement.dispatchEvent(new Event('input'));
+      control.nativeElement.dispatchEvent(new Event('blur'));
+      fixture.detectChanges();
+      expect(valueChanges).toBe(1);
+    });
   });
 }
