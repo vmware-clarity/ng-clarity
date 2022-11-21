vmware-clarity/ng-clarity

commit 1ce653a4bb444395d33c6f9d8f57ebcddb98537a

fix(navigation): a11y focus trap when navigation is open in responsive mode
Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

projects/angular/src/layout/nav/header.ts
@@ -10,6 +10,7 @@ import { Subscription } from 'rxjs';
 import { ResponsiveNavigationService } from './providers/responsive-navigation.service';
 import { ResponsiveNavCodes } from './responsive-nav-codes';
 import { ClrCommonStringsService } from '../../utils/i18n/common-strings.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'clr-header',
@@ -22,6 +23,8 @@ import { ClrCommonStringsService } from '../../utils/i18n/common-strings.service
         openNavLevel !== responsiveNavCodes.NAV_LEVEL_1 ? commonStrings.keys.open : commonStrings.keys.close
       "
       (click)="toggleNav(responsiveNavCodes.NAV_LEVEL_1)"
+      [attr.aria-expanded]="openNavLevel === 1 ? 'true' : 'false'"
+      (click)="openNav(responsiveNavCodes.NAV_LEVEL_1)"
     >
       <span></span>
     </button>
@@ -33,7 +36,8 @@ import { ClrCommonStringsService } from '../../utils/i18n/common-strings.service
       [attr.aria-label]="
         openNavLevel !== responsiveNavCodes.NAV_LEVEL_2 ? commonStrings.keys.open : commonStrings.keys.close
       "
-      (click)="toggleNav(responsiveNavCodes.NAV_LEVEL_2)"
+      [attr.aria-expanded]="openNavLevel === 2 ? 'true' : 'false'"
+      (click)="openNav(responsiveNavCodes.NAV_LEVEL_2)"
     >
       <span></span>
     </button>
@@ -57,6 +61,19 @@ export class ClrHeader implements OnDestroy {
         this.initializeNavTriggers(navLevelList);
       },
     });
+
+    this._subscription.add(
+      this.responsiveNavService.navControl
+        .pipe(
+          filter(
+            ({ controlCode }) =>
+              controlCode === ResponsiveNavCodes.NAV_CLOSE || controlCode === ResponsiveNavCodes.NAV_CLOSE_ALL
+          )
+        )
+        .subscribe(() => {
+          this.openNavLevel = null;
+        })
+    );
   }
 
   // reset triggers. handles cases when an application has different nav levels on different pages.
@@ -86,10 +103,23 @@ export class ClrHeader implements OnDestroy {
     this.responsiveNavService.closeAllNavs();
   }
 
-  // toggles the nav that is open
+  /**
+   * @deprecated Will be removed in with @clr/angular v15.0.0
+   *
+   * Use `openNav(navLevel)` instead to open the navigation and ResponsiveNavService to close it.
+   */
   toggleNav(navLevel: number) {
-    this.openNavLevel = this.openNavLevel === navLevel ? null : navLevel;
-    this.responsiveNavService.sendControlMessage(ResponsiveNavCodes.NAV_TOGGLE, navLevel);
+    if (this.openNavLevel === navLevel) {
+      this.responsiveNavService.sendControlMessage(ResponsiveNavCodes.NAV_CLOSE, navLevel);
+      return;
+    }
+
+    this.openNav(navLevel);
+  }
+
+  openNav(navLevel: number) {
+    this.openNavLevel = navLevel;
+    this.responsiveNavService.sendControlMessage(ResponsiveNavCodes.NAV_OPEN, navLevel);
   }
 
   ngOnDestroy() {

projects/angular/src/layout/nav/_responsive-nav.clarity.scss
@@ -52,6 +52,12 @@
   }
 }
 
+@mixin clr-nav-close-trigger-animation() {
+  left: $clr_baselineRem_0_125;
+  transform-origin: 9%;
+  transition: $clr-trigger-animation;
+}
+
 @mixin overflow-menu-trigger-animation() {
   & > span {
     background: transparent;
@@ -78,12 +84,24 @@
 @import './properties.responsive-nav';
 
 @include exports('responsive-nav.clarity') {
+  .clr-nav-close {
+    margin: 0.75rem;
+
+    &:hover,
+    &:focus {
+      --color: var(--clr-responsive-nav-trigger-bg-color);
+    }
+
+    --color: var(--clr-color-neutral-500);
+  }
+
   .header-hamburger-trigger,
   .header-overflow-trigger {
     display: none;
   }
 
-  .header-hamburger-trigger {
+  .header-hamburger-trigger,
+  .clr-nav-close {
     $trigger-span-psuedo-positioning: -1 * $clr_baselineRem_7px;
 
     & > span,
@@ -131,7 +149,8 @@
     }
   }
 
-  .header-overflow-trigger {
+  .header-overflow-trigger,
+  .clr-nav-close {
     $overflow-trigger-psuedo-positioning: -1 * $clr_baselineRem_8px;
 
     & > span,
@@ -219,6 +238,11 @@
         @include sliding-nav-positioning(0, 0, 0, auto);
         transform: translateX($clr-sliding-panel-width);
         transition: $clr-sliding-panel-animation;
+      }
+
+      .sidenav.clr-nav-level-2 {
+        // This is to prevent the close button to be hidden by the sidenav content
+        overflow: inherit;
       }
 
       .subnav.clr-nav-level-1,
@@ -451,7 +475,7 @@
           }
         }
 
-        .header-hamburger-trigger {
+        .clr-nav-close {
           @include menu-trigger();
           @include hamburger-menu-trigger-animation();
         }
@@ -487,7 +511,7 @@
           padding-top: $clr_baselineRem_1;
         }
 
-        .header-overflow-trigger {
+        .clr-nav-close {
           @include menu-trigger(0, auto, (-1 * $clr-trigger-position));
           @include overflow-menu-trigger-animation();
         }
@@ -562,7 +586,7 @@
           max-width: $clr-sliding-panel-width-sm;
         }
 
-        .header-hamburger-trigger {
+        .clr-nav-close {
           @include menu-trigger(auto, 0, $clr-trigger-position-sm);
         }
       }
@@ -577,7 +601,7 @@
           max-width: $clr-sliding-panel-width-sm;
         }
 
-        .header-overflow-trigger {
+        .clr-nav-close {
           @include menu-trigger(0, auto, (-1 * $clr-trigger-position-sm));
         }
       }

projects/angular/src/layout/nav/nav-level.spec.ts
@@ -4,54 +4,191 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
+import { Component } from '@angular/core';
+import { fakeAsync } from '@angular/core/testing';
+import { CdsInternalCloseButton } from '@cds/core/internal-components/close-button';
+import { LARGE_BREAKPOINT } from 'src/utils/breakpoints/breakpoints';
+import { spec } from '../../utils/testing/helpers.spec';
 import { ClrNavLevel } from './nav-level';
+import { ClrNavigationModule } from './navigation.module';
 import { ResponsiveNavigationService } from './providers/responsive-navigation.service';
 import { ResponsiveNavCodes } from './responsive-nav-codes';
 
-describe('NavLevelDirective', () => {
-  let service: ResponsiveNavigationService;
-  let navLevel: ClrNavLevel;
+@Component({
+  template: ` <nav [clr-nav-level]="1">
+    <a href="javascript:void(0)">Level 1</a>
+  </nav>`,
+})
+class TestComponent {}
 
-  beforeEach(() => {
-    service = new ResponsiveNavigationService();
-    navLevel = new ClrNavLevel(service, null); // null because we are just testing the directive functions
-    navLevel._level = 1;
-  });
+describe('NavLevelDirective', function () {
+  spec(ClrNavLevel, TestComponent, ClrNavigationModule, null, true);
 
-  it('#level is set to 1', () => {
+  it('#level is set to 1', function () {
+    const navLevel = this.clarityDirective;
     expect(navLevel.level).toBe(ResponsiveNavCodes.NAV_LEVEL_1);
   });
 
-  it('#level is set to 2', () => {
+  it('should set [attr.hidden] when called hideNavigation()', function () {
+    const navLevel = this.clarityDirective;
+    navLevel.hideNavigation();
+    const element = this.fixture.nativeElement.querySelector('nav');
+    expect(element.getAttribute('hidden')).toBe('true');
+  });
+
+  it('should remove [attr.hidden] when called showNavigation()', function () {
+    const navLevel = this.clarityDirective;
+    const element = this.fixture.nativeElement.querySelector('nav');
+    navLevel.hideNavigation();
+    expect(element.getAttribute('hidden')).toBe('true');
+    navLevel.showNavigation();
+    expect(element.getAttribute('hidden')).toBeNull();
+  });
+
+  it('should set [attr.hidden] when called hideCloseButton()', function () {
+    const navLevel = this.clarityDirective;
+    const element = this.fixture.nativeElement.querySelector('cds-internal-close-button');
+    navLevel.hideCloseButton();
+    expect(element.getAttribute('hidden')).toBe('true');
+  });
+
+  it('should remove [attr.hidden] when called showCloseButton()', function () {
+    const navLevel = this.clarityDirective;
+    const element = this.fixture.nativeElement.querySelector('cds-internal-close-button');
+    navLevel.hideCloseButton();
+    expect(element.getAttribute('hidden')).toBe('true');
+    navLevel.showCloseButton();
+    expect(element.getAttribute('hidden')).toBeNull();
+  });
+
+  it('should hide the navigation when document client width is smaller than LARGE_BREAKPOINT', function () {
+    this.clarityDirective.onResize({
+      target: {
+        innerWidth: LARGE_BREAKPOINT - 1,
+      },
+    });
+
+    expect(this.clarityDirective.isOpen).toBe(false);
+  });
+
+  it('#level is set to 2', fakeAsync(function () {
+    const navLevel = this.clarityDirective;
     navLevel._level = 2;
     expect(navLevel.level).toBe(ResponsiveNavCodes.NAV_LEVEL_2);
-  });
+  }));
 
-  it('#registers nav on init. sends the registration code on registerNavSubject in the service', () => {
-    service.registerNav(ResponsiveNavCodes.NAV_LEVEL_1);
-    service.registeredNavs.subscribe(navArray => {
-      expect(navArray[0]).toBe(ResponsiveNavCodes.NAV_LEVEL_1);
+  describe('ResponsiveNavLevel intergration:', function () {
+    it('#registers nav on init. sends the registration code on registerNavSubject in the service', function () {
+      const service = new ResponsiveNavigationService();
+
+      service.registerNav(ResponsiveNavCodes.NAV_LEVEL_1);
+      service.registeredNavs.subscribe(navArray => {
+        expect(navArray[0]).toBe(ResponsiveNavCodes.NAV_LEVEL_1);
+      });
+    });
+
+    it('#sends the open code on controlNavSubject in the service when open() is called', function () {
+      const navLevel = this.clarityDirective;
+      const service = this.clarityDirective.responsiveNavService;
+      service.navControl.subscribe(controlMessage => {
+        expect(controlMessage.controlCode).toBe(ResponsiveNavCodes.NAV_OPEN);
+      });
+      navLevel.open();
+    });
+
+    it('#sends the close code on controlNavSubject when close() is called', function () {
+      const navLevel = this.clarityDirective;
+      const service = this.clarityDirective.responsiveNavService;
+      service.navControl.subscribe(controlMessage => {
+        expect(controlMessage.controlCode).toBe(ResponsiveNavCodes.NAV_CLOSE);
+      });
+      navLevel.close();
+    });
+
+    it('#unregisters itself from the registerNavSubject when ngOnDestroy() is called', function () {
+      const navLevel = this.clarityDirective;
+      const service = this.clarityDirective.responsiveNavService;
+      navLevel.ngOnDestroy();
+      service.registeredNavs.subscribe(navArray => {
+        expect(navArray.length).toBe(0);
+      });
     });
   });
 
-  it('#sends the open code on controlNavSubject in the service when open() is called', () => {
-    navLevel.open();
-    service.navControl.subscribe(controlMessage => {
-      expect(controlMessage.controlCode).toBe(ResponsiveNavCodes.NAV_OPEN);
+  describe('Open/Close functionality: ', function () {
+    it('should be open', function () {
+      this.clarityDirective.open();
+      expect(this.clarityDirective.isOpen).toBe(true);
+    });
+
+    describe('Open', function () {
+      it('should not be open by default', function () {
+        expect(this.clarityDirective.isOpen).toBe(false);
+      });
+
+      it('should call showNavigation()', function () {
+        spyOn(this.clarityDirective, 'showNavigation');
+        this.clarityDirective.open();
+        expect(this.clarityDirective.showNavigation).toHaveBeenCalled();
+      });
+
+      it('should call enableFocusTrap()', function () {
+        spyOn(this.clarityDirective, 'enableFocusTrap');
+        this.clarityDirective.open();
+        expect(this.clarityDirective.enableFocusTrap).toHaveBeenCalled();
+      });
+
+      it('should call showCloseButton()', function () {
+        spyOn(this.clarityDirective, 'showCloseButton');
+        this.clarityDirective.open();
+        expect(this.clarityDirective.showCloseButton).toHaveBeenCalled();
+      });
+    });
+
+    it('should be closed', function () {
+      this.clarityDirective.close();
+      expect(this.clarityDirective.isOpen).toBe(false);
+    });
+
+    describe('Close', function () {
+      it('should be closed by default', function () {
+        expect(this.clarityDirective.isOpen).toBe(false);
+      });
+      it('should call hideNavigation()', function () {
+        const spy = spyOn(this.clarityDirective, 'hideNavigation');
+        this.clarityDirective.close();
+        expect(spy).toHaveBeenCalled();
+      });
+      it('should call removeFocusTrap()', function () {
+        const spy = spyOn(this.clarityDirective, 'removeFocusTrap');
+        this.clarityDirective.close();
+        expect(spy).toHaveBeenCalled();
+      });
+      it('should call hideCloseButton()', function () {
+        const spy = spyOn(this.clarityDirective, 'hideCloseButton');
+        this.clarityDirective.close();
+        expect(spy).toHaveBeenCalled();
+      });
     });
   });
 
-  it('#sends the close code on controlNavSubject when close() is called', () => {
-    navLevel.close();
-    service.navControl.subscribe(controlMessage => {
-      expect(controlMessage.controlCode).toBe(ResponsiveNavCodes.NAV_CLOSE);
+  describe('ngAfterViewInit: ', function () {
+    it('should add close button to the host element', function () {
+      const closeButtons = this.fixture.elementRef.nativeElement.querySelectorAll('cds-internal-close-button');
+      expect(closeButtons.length).toBe(1);
+      expect(closeButtons[0]).toBeInstanceOf(CdsInternalCloseButton);
     });
   });
 
-  it('#unregisters itself from the registerNavSubject when ngOnDestroy() is called', () => {
-    navLevel.ngOnDestroy();
-    service.registeredNavs.subscribe(navArray => {
-      expect(navArray.length).toBe(0);
+  describe('Close button:', function () {
+    it('should hide navigation when the close button is clicked', function () {
+      /**
+       * Tried to spyOn on the 'close` function but since we are using `this.close.bind(this)` the spy is not working.
+       */
+      const closeButton = this.fixture.elementRef.nativeElement.querySelector('cds-internal-close-button');
+      this.clarityDirective._isOpen = false;
+      closeButton.click();
+      expect(this.clarityDirective.isOpen).toBe(false);
     });
   });
 });

projects/angular/src/layout/nav/nav-level.ts
@@ -4,24 +4,120 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Inject,
+  Injector,
+  Input,
+  OnInit,
+  PLATFORM_ID,
+  Renderer2,
+} from '@angular/core';
+import { filter } from 'rxjs/operators';
 
 import { ResponsiveNavigationService } from './providers/responsive-navigation.service';
 import { ResponsiveNavCodes } from './responsive-nav-codes';
+import { Subscription } from 'rxjs';
+import { LARGE_BREAKPOINT } from '../../utils/breakpoints/breakpoints';
+import { FocusTrap, FocusTrapElement } from '../../utils/focus-trap/focus-trap';
+import { commonStringsDefault } from '../../utils';
+import '@cds/core/internal-components/close-button/register.js';
+
+const createCdsCloseButton = (document: Document, ariaLabel: string) => {
+  const cdsCloseButton = document.createElement('cds-internal-close-button');
+  cdsCloseButton.setAttribute('icon-size', '32');
+  console.log(ariaLabel);
+  cdsCloseButton.setAttribute('aria-label', ariaLabel);
+  cdsCloseButton.setAttribute('type', 'button');
+  /**
+   * The button is hidden by default based on our Desktop-first approach.
+   */
+  cdsCloseButton.setAttribute('hidden', 'true');
+  cdsCloseButton.className = 'clr-nav-close';
+  return cdsCloseButton;
+};
 
 @Directive({ selector: '[clr-nav-level]' })
-export class ClrNavLevel implements OnInit {
+export class ClrNavLevel extends FocusTrap implements OnInit {
   @Input('clr-nav-level') _level: number;
+  @Input('closeAriaLabel')
+  closeButtonAriaLabel: string;
 
-  constructor(private responsiveNavService: ResponsiveNavigationService, private elementRef: ElementRef) {}
+  private _subscription: Subscription;
+
+  private _isOpen = false;
+
+  constructor(
+    @Inject(PLATFORM_ID) platformId: any,
+    private responsiveNavService: ResponsiveNavigationService,
+    private elementRef: ElementRef<FocusTrapElement>,
+    renderer: Renderer2,
+    injector: Injector
+  ) {
+    super(renderer, injector, platformId, elementRef.nativeElement);
+
+    if (isPlatformBrowser(platformId)) {
+      this._document = injector.get(DOCUMENT);
+    }
+
+    this._subscription = responsiveNavService.navControl
+      .pipe(
+        filter(x => x.navLevel === this.level),
+        filter(
+          ({ controlCode }) =>
+            (controlCode === ResponsiveNavCodes.NAV_OPEN && !this.isOpen) ||
+            (controlCode === ResponsiveNavCodes.NAV_CLOSE && this.isOpen)
+        )
+      )
+      .subscribe(({ controlCode }) => {
+        if (controlCode === ResponsiveNavCodes.NAV_OPEN) {
+          this.open();
+          return;
+        }
+
+        this.close();
+      });
+
+    this._subscription.add(
+      responsiveNavService.navControl
+        .pipe(filter(({ controlCode }) => controlCode === ResponsiveNavCodes.NAV_CLOSE_ALL))
+        .subscribe(() => this.close())
+    );
+  }
 
   ngOnInit() {
+    if (!this.closeButtonAriaLabel) {
+      this.closeButtonAriaLabel =
+        this._level === ResponsiveNavCodes.NAV_LEVEL_1
+          ? commonStringsDefault.responsiveNavToggleClose
+          : commonStringsDefault.responsiveNavOverflowClose;
+    }
+
     if (this.level !== ResponsiveNavCodes.NAV_LEVEL_1 && this.level !== ResponsiveNavCodes.NAV_LEVEL_2) {
       console.error('Nav Level can only be 1 or 2');
       return;
     }
     this.responsiveNavService.registerNav(this.level);
     this.addNavClass(this.level);
+  }
+
+  ngAfterViewInit() {
+    const closeButton = createCdsCloseButton(this._document, this.closeButtonAriaLabel);
+    this.renderer.listen(closeButton, 'click', this.close.bind(this));
+    this.renderer.insertBefore(this.elementRef.nativeElement, closeButton, this.elementRef.nativeElement.firstChild); // Adding the button at the top of the nav
+
+    if (this._document.body.clientWidth < LARGE_BREAKPOINT) {
+      /**
+       * Close if the document body is smaller than the large breakpoint for example:
+       * - Refreshing the page
+       * - Browser window size is changed when opening the applicaiton
+       * - Browser zoom is turned on and zoomed to a size that makes the document smaller than the large breakpoint
+       */
+      this.close();
+    }
   }
 
   addNavClass(level: number) {
@@ -42,11 +138,35 @@ export class ClrNavLevel implements OnInit {
     return ResponsiveNavCodes;
   }
 
+  get isOpen(): boolean {
+    return this._isOpen;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const target = event.target as Window;
+
+    if (target.innerWidth < LARGE_BREAKPOINT && this.isOpen) {
+      this.close();
+      return;
+    }
+
+    this.showNavigation();
+  }
+
   open(): void {
+    this._isOpen = true;
+    this.showNavigation();
+    this.enableFocusTrap();
+    this.showCloseButton();
     this.responsiveNavService.sendControlMessage(ResponsiveNavCodes.NAV_OPEN, this.level);
   }
 
   close(): void {
+    this._isOpen = false;
+    this.hideNavigation();
+    this.removeFocusTrap();
+    this.hideCloseButton();
     this.responsiveNavService.sendControlMessage(ResponsiveNavCodes.NAV_CLOSE, this.level);
   }
 
@@ -72,7 +192,24 @@ export class ClrNavLevel implements OnInit {
     }
   }
 
+  protected hideNavigation() {
+    this.renderer.setAttribute(this.elementRef.nativeElement, 'hidden', 'true');
+  }
+
+  protected showNavigation() {
+    this.renderer.removeAttribute(this.elementRef.nativeElement, 'hidden');
+  }
+
+  protected hideCloseButton() {
+    this.renderer.setAttribute(this.elementRef.nativeElement.querySelector('.clr-nav-close'), 'hidden', 'true');
+  }
+
+  protected showCloseButton() {
+    this.renderer.removeAttribute(this.elementRef.nativeElement.querySelector('.clr-nav-close'), 'hidden');
+  }
+
   ngOnDestroy() {
     this.responsiveNavService.unregisterNav(this.level);
+    this._subscription.unsubscribe();
   }
 }

projects/angular/src/utils/breakpoints/breakpoints.ts
@@ -4,6 +4,21 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
+/**
+ * TODO:
+ * Using core functions like:
+ * - pluckPixelValue
+ * - getCssPropertyValue
+ *
+ * to get the value of the design token.
+ *
+ * Note: Memoization/Cache usage possible.
+ */
+
 // iPad mini screen width
 // http://stephen.io/mediaqueries/#iPadMini
 export const DATEPICKER_ENABLE_BREAKPOINT = 768;
+export const SMALL_BREAKPOINT = 576;
+export const MEDIUM_BREAKPOINT = 768;
+export const LARGE_BREAKPOINT = 992;
+export const EXTRA_LARGE_BREAKPOINT = 1200;

commit 5909fcaf31f2659a8bcc4a0fe406fd29aa2b647f

fix(header): remove incorrect, duplicate `click` event handler
This was accidentally introduced in 1ce653a4bb444395d33c6f9d8f57ebcddb98537a.

projects/angular/src/layout/nav/header.ts
@@ -22,7 +22,6 @@ import { ResponsiveNavCodes } from './responsive-nav-codes';
       [attr.aria-label]="
         openNavLevel !== responsiveNavCodes.NAV_LEVEL_1 ? commonStrings.keys.open : commonStrings.keys.close
       "
-      (click)="toggleNav(responsiveNavCodes.NAV_LEVEL_1)"
       [attr.aria-expanded]="openNavLevel === 1 ? 'true' : 'false'"
       (click)="openNav(responsiveNavCodes.NAV_LEVEL_1)"
     >