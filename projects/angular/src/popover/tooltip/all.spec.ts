ed60e16ebc94f48ddd536da465fee1027f4637ef
Kevin Buhmann <kbuhmann@vmware.com>
2023-01-05 20:38:05 -0600

    chore(tooltip): execute `TooltipIdService` tests

--- projects/angular/src/popover/tooltip/all.spec.ts
+++ projects/angular/src/popover/tooltip/all.spec.ts
@@ -4,11 +4,13 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
+import TooltipIdServiceSpecs from './providers/tooltip-id.service.spec';
 import TooltipContentSpecs from './tooltip-content.spec';
 import TooltipTriggerSpecs from './tooltip-trigger.spec';
 import TooltipSpecs from './tooltip.spec';
 
 describe('Tooltip', () => {
+  TooltipIdServiceSpecs();
   TooltipContentSpecs();
   TooltipTriggerSpecs();
   TooltipSpecs();
