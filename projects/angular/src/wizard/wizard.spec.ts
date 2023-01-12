vmware-clarity/ng-clarity

commit 7c811ae7dc78231a877e4c4f1d0f551cf9cb531a

fix(modal): capture and return focus
Focus is captured on a focusable element in the modal when it opened,
and focus is returned to the previously-focused element when it is
closed.

This change contributes to fixing VPAT-735.

projects/angular/src/modal/modal.html
@@ -4,7 +4,7 @@
   ~ The full license information can be found in LICENSE in the root directory of this project.
   -->
 
-<div cdkTrapFocus class="modal" *ngIf="_open">
+<div cdkTrapFocus [cdkTrapFocusAutoCapture]="true" class="modal" *ngIf="_open">
   <!--fixme: revisit when ngClass works with exit animation-->
   <div
     [@fadeDown]="skipAnimation"

commit b5fb9595c95f4acbcc47578c0be6c857ace31640

fix(modal): focus modal title when modal opened
The tests need to wait for stable so that `cdkFocusInitial` works
properly and doesn't log a warning.

This fixes a regression I introduced in 7c811ae7dc78231a877e4c4f1d0f551cf9cb531a.

projects/angular/src/modal/modal.html
@@ -25,7 +25,7 @@
 
       <div class="modal-content">
         <div class="modal-header--accessible">
-          <div class="modal-title-wrapper" id="{{modalId}}" clrFocusOnViewInit>
+          <div class="modal-title-wrapper" id="{{modalId}}" cdkFocusInitial tabindex="-1">
             <ng-content select=".modal-title"></ng-content>
           </div>
           <button

projects/angular/src/modal/modal.spec.ts
@@ -67,7 +67,7 @@ describe('Modal', () => {
   let modal: ClrModal;
   const commonStrings = new ClrCommonStringsService();
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [CdkTrapFocusModule, ClrModalModule, NoopAnimationsModule],
       declarations: [TestComponent, TestDefaultsComponent],
@@ -77,6 +77,8 @@ describe('Modal', () => {
     fixture.detectChanges();
     compiled = fixture.nativeElement;
     modal = fixture.componentInstance.modalInstance;
+
+    await fixture.whenStable();
   });
 
   function flushAndExpectOpen(componentFixture: ComponentFixture<any>, open: boolean): void {

projects/angular/src/wizard/wizard.spec.ts
@@ -23,11 +23,13 @@ export default function (): void {
         let wizard: ClrWizard;
         let component: UnopenedWizardTestComponent;
 
-        beforeEach(function () {
+        beforeEach(async function () {
           context = this.create(ClrWizard, UnopenedWizardTestComponent);
           wizard = context.clarityDirective;
           component = context.hostComponent;
           context.detectChanges();
+
+          await context.fixture.whenStable();
         });
 
         describe('open', () => {
@@ -105,12 +107,14 @@ export default function (): void {
         let pageCollectionService: PageCollectionService;
         let wizard: ClrWizard;
 
-        beforeEach(function () {
+        beforeEach(async function () {
           context = this.create(ClrWizard, BasicWizardTestComponent);
           wizardNavigationService = context.getClarityProvider(WizardNavigationService);
           pageCollectionService = context.getClarityProvider(PageCollectionService);
           wizard = context.clarityDirective;
           context.detectChanges();
+
+          await context.fixture.whenStable();
         });
 
         describe('currentPage', () => {
@@ -234,10 +238,12 @@ export default function (): void {
       let context: TestContext<ClrWizard, TemplateApiWizardTestComponent>;
       let wizard: ClrWizard;
 
-      beforeEach(function () {
+      beforeEach(async function () {
         context = this.create(ClrWizard, TemplateApiWizardTestComponent);
         wizard = context.clarityDirective;
         context.detectChanges();
+
+        await context.fixture.whenStable();
       });
 
       describe('Current page onchange', () => {
@@ -616,10 +622,12 @@ export default function (): void {
       let context: TestContext<ClrWizard, DynamicWizardTestComponent>;
       let wizard: ClrWizard;
 
-      beforeEach(function () {
+      beforeEach(async function () {
         context = this.create(ClrWizard, DynamicWizardTestComponent);
         wizard = context.clarityDirective;
         context.detectChanges();
+
+        await context.fixture.whenStable();
       });
 
       it('pages can be added via an ngFor', () => {
@@ -726,10 +734,12 @@ export default function (): void {
       let context: TestContext<ClrWizard, TemplateApiWizardTestComponent>;
       let wizard: ClrWizard;
 
-      beforeEach(function () {
+      beforeEach(async function () {
         context = this.create(ClrWizard, TemplateApiWizardTestComponent);
         wizard = context.clarityDirective;
         context.detectChanges();
+
+        await context.fixture.whenStable();
       });
 
       describe('Page title focus', () => {
@@ -746,10 +756,12 @@ export default function (): void {
       let context: TestContext<ClrWizard, TemplateApiWizardTestComponent>;
       let wizard: ClrWizard;
 
-      beforeEach(function () {
+      beforeEach(async function () {
         context = this.create(ClrWizard, TemplateApiWizardTestComponent);
         wizard = context.clarityDirective;
         context.detectChanges();
+
+        await context.fixture.whenStable();
       });
 
       describe('aria-labelledby', () => {