vmware-clarity/ng-clarity

commit 4828c5c8839aae6f50df358f18ba715af0fbf03d

feat: use `cdkTrapFocus` directive from `@angular/cdk`

projects/angular/src/data/datagrid/datagrid.module.ts
@@ -25,8 +25,8 @@ import { ClrIconModule } from '../../icon/icon.module';
 import { ClrSpinnerModule } from '../../progress/spinner/spinner.module';
 import { ClrExpandableAnimationModule } from '../../utils/animations/expandable-animation/expandable-animation.module';
 import { CdkDragModule } from '../../utils/cdk/cdk-drag.module';
+import { CdkTrapFocusModule } from '../../utils/cdk/cdk-trap-focus.module';
 import { ClrConditionalModule } from '../../utils/conditional/conditional.module';
-import { ClrFocusTrapModule } from '../../utils/focus-trap/focus-trap.module';
 import { ClrFocusOnViewInitModule } from '../../utils/focus/focus-on-view-init/focus-on-view-init.module';
 import { ClrKeyFocusModule } from '../../utils/focus/key-focus/key-focus.module';
 import { ClrLoadingModule } from '../../utils/loading/loading.module';
@@ -119,6 +119,7 @@ export const CLR_DATAGRID_DIRECTIVES: Type<any>[] = [
   imports: [
     CommonModule,
     CdkDragModule,
+    CdkTrapFocusModule,
     ClrIconModule,
     ClrFormsModule,
     FormsModule,
@@ -129,7 +130,6 @@ export const CLR_DATAGRID_DIRECTIVES: Type<any>[] = [
     ClrSpinnerModule,
     ClrPopoverModuleNext,
     ClrKeyFocusModule,
-    ClrFocusTrapModule,
     ClrFocusOnViewInitModule,
   ],
   declarations: [CLR_DATAGRID_DIRECTIVES],

projects/angular/src/data/datagrid/datagrid-action-overflow.ts
@@ -57,7 +57,7 @@ let clrDgActionId = 0;
       [attr.aria-hidden]="!open"
       [attr.id]="popoverId"
       clrKeyFocus
-      clrFocusTrap
+      cdkTrapFocus
       (click)="closeOverflowContent($event)"
       *clrPopoverContent="open; at: smartPosition; outsideClickToClose: true; scrollToClose: true"
     >

projects/angular/src/data/datagrid/datagrid-column-toggle.ts
@@ -42,7 +42,7 @@ import { ColumnsService } from './providers/columns.service';
       role="dialog"
       [attr.aria-label]="commonStrings.keys.showColumnsMenuDescription"
       [id]="popoverId"
-      clrFocusTrap
+      cdkTrapFocus
       *clrPopoverContent="openState; at: smartPosition; outsideClickToClose: true; scrollToClose: true"
     >
       <div class="switch-header">

projects/angular/src/data/datagrid/datagrid-detail.spec.ts
@@ -65,13 +65,13 @@ export default function (): void {
       }));
 
       it('conditionally enables focus trap when opened', () => {
-        expect(context.clarityElement.innerHTML).not.toContain('clrfocustrap');
+        expect(context.clarityElement.innerHTML).not.toContain('cdkfocustrap');
         detailService.open({});
         context.detectChanges();
-        expect(context.clarityElement.innerHTML).toContain('clrfocustrap');
+        expect(context.clarityElement.innerHTML).toContain('cdkfocustrap');
         detailService.close();
         context.detectChanges();
-        expect(context.clarityElement.innerHTML).not.toContain('clrfocustrap');
+        expect(context.clarityElement.innerHTML).not.toContain('cdkfocustrap');
       });
 
       it('should have text based boundaries for screen readers', () => {

projects/angular/src/data/datagrid/datagrid-detail.ts
@@ -15,11 +15,12 @@ import { DetailService } from './providers/detail.service';
   host: {
     '[class.datagrid-detail-pane]': 'true',
   },
-  // We put the *ngIf on the clrFocusTrap so it doesn't always exist on the page
+  // We put the *ngIf on the cdkTrapFocus so it doesn't always exist on the page
   // have to test for presence of header for aria-describedby because it was causing unit tests to crash
   template: `
     <div
-      [clrFocusTrap]="{ strict: false }"
+      cdkTrapFocus
+      [cdkTrapFocusAutoCapture]="true"
       class="datagrid-detail-pane-content"
       *ngIf="detailService.isOpen"
       role="dialog"

projects/angular/src/data/datagrid/datagrid-filter.ts
@@ -63,7 +63,7 @@ import { DatagridFilterRegistrar } from './utils/datagrid-filter-registrar';
     <div
       class="datagrid-filter"
       [id]="popoverId"
-      clrFocusTrap
+      cdkTrapFocus
       *clrPopoverContent="open; at: smartPosition; outsideClickToClose: true; scrollToClose: true"
       role="dialog"
       [attr.aria-label]="commonStrings.keys.datagridFilterDialogAriaLabel"

projects/angular/src/forms/datepicker/date-container.ts
@@ -49,7 +49,7 @@ import { ViewManagerService } from './providers/view-manager.service';
           </button>
           <clr-datepicker-view-manager
             *clrPopoverContent="open; at: popoverPosition; outsideClickToClose: true; scrollToClose: true"
-            clrFocusTrap
+            cdkTrapFocus
           ></clr-datepicker-view-manager>
         </div>
         <cds-icon

projects/angular/src/forms/datepicker/datepicker.module.ts
@@ -16,8 +16,8 @@ import {
 } from '@cds/core/icon';
 
 import { ClrIconModule } from '../../icon/icon.module';
+import { CdkTrapFocusModule } from '../../utils/cdk/cdk-trap-focus.module';
 import { ClrConditionalModule } from '../../utils/conditional/conditional.module';
-import { ClrFocusTrapModule } from '../../utils/focus-trap/focus-trap.module';
 import { ClrHostWrappingModule } from '../../utils/host-wrapping/host-wrapping.module';
 import { ClrPopoverModuleNext } from '../../utils/popover/popover.module';
 import { ClrCommonFormsModule } from '../common/common.module';
@@ -46,11 +46,11 @@ export const CLR_DATEPICKER_DIRECTIVES: Type<any>[] = [
 @NgModule({
   imports: [
     CommonModule,
+    CdkTrapFocusModule,
     ClrHostWrappingModule,
     ClrConditionalModule,
     ClrPopoverModuleNext,
     ClrIconModule,
-    ClrFocusTrapModule,
     ClrCommonFormsModule,
   ],
   declarations: [CLR_DATEPICKER_DIRECTIVES],

commit 21f29f5915409160759247d9005f9c3853e7c686

fix(datagrid): focus detail pane header when opened
This fixes a regression I introduced in 4828c5c8839aae6f50df358f18ba715af0fbf03d.

projects/angular/src/data/datagrid/datagrid-detail-header.ts
@@ -15,7 +15,7 @@ import { DetailService } from './providers/detail.service';
     '[class.datagrid-detail-header]': 'true',
   },
   template: `
-    <div class="datagrid-detail-header-title" clrFocusOnViewInit [id]="titleId">
+    <div class="datagrid-detail-header-title" cdkFocusInitial tabindex="-1" [id]="titleId">
       <ng-content></ng-content>
     </div>
     <div class="datagrid-detail-pane-close">

projects/angular/src/data/datagrid/datagrid.module.ts
@@ -27,7 +27,6 @@ import { ClrExpandableAnimationModule } from '../../utils/animations/expandable-
 import { CdkDragModule } from '../../utils/cdk/cdk-drag.module';
 import { CdkTrapFocusModule } from '../../utils/cdk/cdk-trap-focus.module';
 import { ClrConditionalModule } from '../../utils/conditional/conditional.module';
-import { ClrFocusOnViewInitModule } from '../../utils/focus/focus-on-view-init/focus-on-view-init.module';
 import { ClrKeyFocusModule } from '../../utils/focus/key-focus/key-focus.module';
 import { ClrLoadingModule } from '../../utils/loading/loading.module';
 import { ClrOutsideClickModule } from '../../utils/outside-click/outside-click.module';
@@ -130,7 +129,6 @@ export const CLR_DATAGRID_DIRECTIVES: Type<any>[] = [
     ClrSpinnerModule,
     ClrPopoverModuleNext,
     ClrKeyFocusModule,
-    ClrFocusOnViewInitModule,
   ],
   declarations: [CLR_DATAGRID_DIRECTIVES],
   exports: [CLR_DATAGRID_DIRECTIVES],