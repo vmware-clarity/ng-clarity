1c8fc6b2351d339a3cdecca96aefad55cf1b864e
Daniel Tsanev <tsanevd@vmware.com>
2023-03-16 13:23:38 +0200

    fix(button): custom aria label for button group menu toggle

--- .storybook/stories/button/button-group.stories.ts
+++ .storybook/stories/button/button-group.stories.ts
@@ -4,7 +4,7 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import { CLR_MENU_POSITIONS, ClrButtonGroup, ClrButtonGroupModule } from '@clr/angular';
+import { CLR_MENU_POSITIONS, ClrButtonGroup, ClrButtonGroupModule, ClrCommonStringsService } from '@clr/angular';
 import { Parameters } from '@storybook/addons';
 import { Story } from '@storybook/angular';
 
@@ -13,7 +13,7 @@ import { setupStorybook } from '../../helpers/setup-storybook.helpers';
 const defaultStory: Story = args => ({
   template: `
     <div style="margin-top: 200px; text-align: center;">
-      <clr-button-group [clrMenuPosition]="clrMenuPosition">
+      <clr-button-group [clrMenuPosition]="clrMenuPosition" [clrToggleButtonAriaLabel]="clrToggleButtonAriaLabel">
         <clr-button
           *ngFor="let _ of createArray(buttonCount); let i = index"
           [clrInMenu]="false"
@@ -32,6 +32,8 @@ const defaultStory: Story = args => ({
   props: { ...args },
 });
 
+const commonStringsService = new ClrCommonStringsService();
+
 const defaultParameters: Parameters = {
   title: 'Button/Button Group',
   component: ClrButtonGroup,
@@ -47,6 +49,8 @@ const defaultParameters: Parameters = {
     createArray: { control: { disable: true }, table: { disable: true } },
     buttonCount: { control: { type: 'number', min: 1, max: 100 } },
     inMenuButtonCount: { control: { type: 'number', min: 1, max: 100 } },
+    // aria
+    clrToggleButtonAriaLabel: { defaultValue: commonStringsService.keys.rowActions },
   },
   args: {
     // story helpers
