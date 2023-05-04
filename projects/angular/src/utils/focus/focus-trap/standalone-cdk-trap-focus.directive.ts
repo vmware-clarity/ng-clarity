f1d624b5c4c423d61f64fb744a9d75872bb5c075
Kevin Buhmann <kbuhmann@vmware.com>
2023-05-04 08:47:22 -0500

    fix(nav): export standalone `CdkTrapFocus` directive
    This allows the Angular compiler to properly type check templates that
    have components that use the standalone `CdkTrapFocus` directive.
    
    part of #666

---
+++ projects/angular/src/utils/focus/focus-trap/standalone-cdk-trap-focus.directive.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016-2023 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { CdkTrapFocus } from '@angular/cdk/a11y';
+import { Directive } from '@angular/core';
+
+@Directive({
+  standalone: true,
+})
+export class ClrStandaloneCdkTrapFocus extends CdkTrapFocus {}
