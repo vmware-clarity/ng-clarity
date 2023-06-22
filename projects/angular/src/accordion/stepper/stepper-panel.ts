6862ceadae84b2232919405f1654c372ff60261f
mivaylo <mivaylo@vmware.com>
2023-06-22 16:08:14 +0300

    fix: (stepper) revert linter change as requested in review

--- projects/angular/src/accordion/stepper/stepper-panel.ts
+++ projects/angular/src/accordion/stepper/stepper-panel.ts
@@ -20,9 +20,9 @@ import { FormGroupName, NgModelGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { filter, pairwise, tap } from 'rxjs/operators';
 
-import { ClrCommonStringsService } from '../../utils';
 import { IfExpandService } from '../../utils/conditional/if-expanded.service';
 import { triggerAllFormControlValidation } from '../../utils/forms/validation';
+import { ClrCommonStringsService } from '../../utils/i18n/common-strings.service';
 import { ClrAccordionPanel } from '../accordion-panel';
 import { AccordionStatus } from '../enums/accordion-status.enum';
 import { AccordionPanelModel } from '../models/accordion.model';
