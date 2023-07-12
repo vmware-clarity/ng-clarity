c038bfedca2025b785cbf47f974e688b850d6e25
Valentin Mladenov <mvalentin@vmware.com>
2023-07-12 11:38:58 +0300

    fix: recalculate some container positions

--- projects/demo/src/app/signpost/signpost.demo.ts
+++ projects/demo/src/app/signpost/signpost.demo.ts
@@ -14,17 +14,18 @@ import { Component } from '@angular/core';
 export class SignpostDemo {
   openState = false;
   positions = [
-    { description: 'Right bottom', position: 'right-bottom', id: 'rightBottomTrigger' },
-    { description: 'Bottom middle', position: 'bottom-middle', id: 'bottomMiddleTrigger' },
-    { description: 'Left bottom', position: 'left-bottom', id: 'leftBottomTrigger' },
-    { description: 'Right middle', position: 'right-middle', id: 'rightMiddleTrigger' },
-    { description: 'Left middle', position: 'left-middle', id: 'leftMiddleTrigger' },
-    { description: 'Left top', position: 'left-top', id: 'leftTopTrigger' },
-    { description: 'Right top', position: 'right-top', id: 'rightTopTrigger' },
-    { description: 'Bottom right', position: 'bottom-right', id: 'bottomRightTrigger' },
-    { description: 'Bottom left', position: 'bottom-left', id: 'bottomLeftTrigger' },
-    { description: 'Top right', position: 'top-right', id: 'topRightTrigger' },
-    { description: 'Top middle', position: 'top-middle', id: 'topMiddleTrigger' },
-    { description: 'Top left', position: 'top-left', id: 'topLeftTrigger' },
+    { icon: 'align-left', text: '', description: 'Top left', position: 'top-left', id: 'topLeftTrigger' },
+    { icon: 'yen', text: '', description: 'Top middle', position: 'top-middle', id: 'topMiddleTrigger' },
+    { icon: 'world', text: '', description: 'Top right', position: 'top-right', id: 'topRightTrigger' },
+    { icon: 'user', text: '', description: 'Left top', position: 'left-top', id: 'leftTopTrigger' },
+    { icon: 'search', text: 'Error', description: 'Left middle', position: 'left-middle', id: 'leftMiddleTrigger' },
+    { icon: 'bell', text: 'Some text', description: 'Left bottom', position: 'left-bottom', id: 'leftBottomTrigger' },
+    { icon: 'cloud', text: 'Warning', description: 'Right top', position: 'right-top', id: 'rightTopTrigger' },
+    { icon: 'eye', text: '', description: 'Right middle', position: 'right-middle', id: 'rightMiddleTrigger' },
+    { icon: 'help-info', text: '', description: 'Right bottom', position: 'right-bottom', id: 'rightBottomTrigger' },
+    { icon: 'pencil', text: '', description: 'Bottom left', position: 'bottom-left', id: 'bottomLeftTrigger' },
+    { icon: 'info', text: 'Info', description: 'Bottom middle', position: 'bottom-middle', id: 'bottomMiddleTrigger' },
+    { icon: 'bolt', text: '', description: 'Bottom right', position: 'bottom-right', id: 'bottomRightTrigger' },
+    { icon: 'success-standard', text: 'Default', description: 'Default', position: 'default', id: 'defaultTrigger' },
   ];
 }
