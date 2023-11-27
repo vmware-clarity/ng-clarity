adada612be36465728351f9b4d7fddfff8ee672d
mivaylo <mivaylo@vmware.com>
2023-11-27 10:28:24 +0200

    chore(github): run visual tests in firefox

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Firefox'] },
     },
   ],
   webServer: {
