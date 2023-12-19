349cfb462771c1ec654c5b546fc00ff26feba530
mivaylo <mivaylo@vmware.com>
2023-12-19 14:52:07 +0200

    fix(storybook): improve naming

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -24,7 +24,7 @@ const privateModifier = 121;
 const cdsThemeAttribute = 'cds-theme';
 const styleElement = addStyleElement();
 
-const cdsCoreAndShimStyles = [previewStyles, resetStyles, coreTokens, darkThemeCoreStyles, clrUiStyles];
+const cdsCoreStyles = [previewStyles, resetStyles, coreTokens, darkThemeCoreStyles, clrUiStyles];
 
 loadIcons();
 addDocs(docs);
@@ -108,7 +108,7 @@ export const globalTypes = {
 const themeDecorator = (story, { globals }) => {
   const { theme } = globals;
 
-  styleElement.textContent = `${cdsCoreAndShimStyles.join('')}`;
+  styleElement.textContent = `${cdsCoreStyles.join('')}`;
   document.body.setAttribute(cdsThemeAttribute, theme);
 
   return story();
