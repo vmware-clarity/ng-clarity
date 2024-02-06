87b274b64e578ccc153fb5b36f1d1af458f3d87b
Andrea Fernandes <andreaf1@vmware.com>
2024-02-06 17:01:24 +0530

    chore(label): update stories for labels
    - [Storybook] - Refactor label stories to CSFv3
    
    Signed-off-by: Andrea Fernandes <andrea.fernandes@broadcom.com>

--- .storybook/stories/label/label.stories.ts
+++ .storybook/stories/label/label.stories.ts
@@ -4,58 +4,99 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import { Parameters } from '@storybook/addons';
-import { Story } from '@storybook/angular';
+import { Story, StoryObj } from '@storybook/angular';
 
-import { setupStorybook } from '../../helpers/setup-storybook.helpers';
+const LABEL_COLOR_TYPES = ['', 'label-purple', 'label-blue', 'label-orange', 'label-light-blue'];
 
-enum LabelType {
-  Default = '',
-  Purple = 'label-purple',
-  Blue = 'label-blue',
-  Orange = 'label-orange',
-  LightBlue = 'label-light-blue',
-  Info = 'label-info',
-  Success = 'label-success',
-  Warning = 'label-warning',
-  Error = 'label-danger',
-}
+const LABEL_STATUS_TYPES = ['label-info', 'label-success', 'label-warning', 'label-danger'];
 
-const defaultStory: Story = args => ({
-  template: `
-    <span class="label" [class.clickable]="clickable" [ngClass]="labelType">
-      <span class="text">{{content}}</span>
-      <span *ngIf="badgeContent" class="badge">{{badgeContent}}</span>
-      <cds-icon *ngIf="closeIcon" shape="close"></cds-icon>
-    </span>
-  `,
-  props: { ...args },
-});
-
-const defaultParameters: Parameters = {
+export default {
   title: 'Label/Label',
   argTypes: {
     // story helpers
-    labelType: { control: { type: 'radio', options: Object.values(LabelType) } },
+    labelType: {
+      defaultValue: '',
+      control: { type: 'radio', options: [...LABEL_COLOR_TYPES, ...LABEL_STATUS_TYPES] },
+    },
+    LABEL_COLOR_TYPES: { control: { disable: true }, table: { disable: true }, type: 'array' },
+    LABEL_STATUS_TYPES: { control: { disable: true }, table: { disable: true }, type: 'array' },
   },
   args: {
     // story helpers
     content: 'Hello World!',
     badgeContent: '',
-    labelType: LabelType.Default,
+    labelType: '',
     clickable: false,
     closeIcon: false,
+    LABEL_COLOR_TYPES,
+    LABEL_STATUS_TYPES,
   },
 };
 
-const variants: Parameters[] = [].concat(
-  ...Object.values(LabelType).map(labelType => [
-    {
-      labelType,
-    },
-    { labelType, badgeContent: '12' },
-    { labelType, badgeContent: '99+', clickable: true, closeIcon: true },
-  ])
-);
+const LabelTemplate: Story = args => ({
+  template: `
+      <span class="label" [class.clickable]="clickable" [ngClass]="labelType">
+        <span class="text">{{content}}</span>
+        <span *ngIf="badgeContent" class="badge">{{badgeContent}}</span>
+        <cds-icon *ngIf="closeIcon" shape="close"></cds-icon>
+      </span>
+  `,
+  props: args,
+});
 
-setupStorybook([], defaultStory, defaultParameters, variants);
+const LabelAllTemplate: Story = args => ({
+  template: `
+    <h6>Default Label</h6>
+    <div style="margin-top: 5px;">
+      <span class="label">{{content}}</span>
+    </div>
+
+    <h6>Color Options</h6>
+    <div style="margin-top: 5px;">
+      <span class="label" *ngFor="let type of LABEL_TYPES" [class]="type">{{content}}</span>
+    </div>
+
+    <h6>Clickable Labels</h6>
+    <div style="margin-top: 5px;">
+      <a class="label clickable" *ngFor="let type of LABEL_COLOR_TYPES" href="javascript://" [class]="type">{{content}}</a>
+    </div>
+
+    <h6>Status Labels</h6>
+    <div style="margin-top: 5px;">
+      <span class="label" *ngFor="let type of LABEL_STATUS_TYPES" [class]="type">{{content}}</span>
+    </div>
+
+    <h6>Labels with Badges</h6>
+    <div style="margin-top: 5px;">
+      <a href="javascript://" class="label clickable" *ngFor="let type of LABEL_TYPES" [class]="type">
+        <span class="text">{{content}}</span>
+        <span class="badge">1</span>
+      </a>
+    </div>
+
+    <h6>Labels with Close Icon</h6>
+    <div style="margin-top: 5px;">
+      <a href="javascript://" class="label clickable" *ngFor="let type of LABEL_TYPES" [class]="type">
+        <span class="text">{{content}}</span>
+        <cds-icon shape="close"></cds-icon>
+      </a>
+    </div>
+  `,
+  props: args,
+});
+
+export const Label: StoryObj = {
+  render: LabelTemplate,
+};
+
+export const Showcase: StoryObj = {
+  render: LabelAllTemplate,
+  args: {
+    ...LabelTemplate.args,
+    LABEL_TYPES: [...LABEL_COLOR_TYPES, ...LABEL_STATUS_TYPES],
+  },
+  parameters: {
+    actions: { disable: true },
+    controls: { disable: true },
+  },
+};
