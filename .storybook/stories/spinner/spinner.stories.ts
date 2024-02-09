6eb1a9fc680bacc2cc4539987ec8eb2278ccc31c
Andrea Fernandes <andreaf1@vmware.com>
2024-02-09 12:14:36 +0530

    chore(spinner): update stories for spinner
    - CDE-1540 [Storybook] - Refactor spinner stories to CSFv3
    
    Signed-off-by: Andrea Fernandes <andrea.fernandes@broadcom.com>

--- .storybook/stories/spinner/spinner.stories.ts
+++ .storybook/stories/spinner/spinner.stories.ts
@@ -49,41 +49,96 @@ const SpinnerTemplate: Story = args => ({
   props: args,
 });
 
+const SpinnerAllTemplate: Story = args => ({
+  template: `
+  <div style="text-align: center">
+    <h5>Large Spinners</h5>
+    
+    <h6>Default</h6>
+    <clr-spinner> {{text}} </clr-spinner>
+    <h6>Inverse</h6>
+    <clr-spinner [clrInverse]="true"> {{text}} </clr-spinner>
+  
+    <h5>Medium Spinners</h5>
+
+    <h6>Default</h6>
+    <div style="margin-top:5px">
+      <clr-spinner [clrMedium]="true"> {{text}} </clr-spinner>
+    </div>
+    <h6>Inverse</h6>
+    <div style="margin-top:5px">
+      <clr-spinner [clrMedium]="true" [clrInverse]="true"> {{text}} </clr-spinner>
+    </div> 
+
+    <h5>Small Spinners</h5>
+
+    <h6>Default</h6>
+    <div style="margin-top:5px">
+      <clr-spinner [clrSmall]="true"> {{text}} </clr-spinner>
+    </div>
+    <h6>Inverse</h6>
+    <div style="margin-top:5px">
+      <clr-spinner [clrSmall]="true" [clrInverse]="true"> {{text}} </clr-spinner>
+    </div>
+
+    <h5>Inline Spinners</h5>
+
+    <h6>Default</h6>
+    <div style="margin-top:5px">
+      <clr-spinner [clrInline]="true"></clr-spinner> {{text}}
+    </div>
+    <h6>Inverse</h6>
+    <div style="margin-top:5px">
+      <clr-spinner [clrInline]="true" [clrInverse]="true"></clr-spinner> {{text}}
+    </div>
+  </div>
+  `,
+  props: args,
+});
+
 export const Spinner: StoryObj = {
   render: SpinnerTemplate,
 };
 
-export const Inverse: StoryObj = {
+export const SpinnerInverse: StoryObj = {
   render: SpinnerTemplate,
   args: { clrInverse: true },
 };
 
-export const Medium: StoryObj = {
+export const SpinnerMedium: StoryObj = {
   render: SpinnerTemplate,
   args: { clrMedium: true },
 };
 
-export const MediumInverse: StoryObj = {
+export const SpinnerMediumInverse: StoryObj = {
   render: SpinnerTemplate,
   args: { clrMedium: true, clrInverse: true },
 };
 
-export const Small: StoryObj = {
+export const SpinnerSmall: StoryObj = {
   render: SpinnerTemplate,
   args: { clrSmall: true },
 };
 
-export const SmallInverse: StoryObj = {
+export const SpinnerSmallInverse: StoryObj = {
   render: SpinnerTemplate,
   args: { clrSmall: true, clrInverse: true },
 };
 
-export const Inline: StoryObj = {
+export const SpinnerInline: StoryObj = {
   render: SpinnerTemplate,
   args: { clrInline: true },
 };
 
-export const InlineInverse: StoryObj = {
+export const SpinnerInlineInverse: StoryObj = {
   render: SpinnerTemplate,
   args: { clrInline: true, clrInverse: true },
+};
+
+export const SpinnerShowcase: StoryObj = {
+  render: SpinnerAllTemplate,
+  parameters: {
+    actions: { disable: true },
+    controls: { disable: true },
+  },
 };
