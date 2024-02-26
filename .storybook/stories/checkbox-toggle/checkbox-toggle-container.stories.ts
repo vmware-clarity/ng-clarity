a15025dc27a89c080a97ed3d4e11b4cc67edad5e
Andrea A Fernandes <andy2890@gmail.com>
2024-02-26 10:09:40 +0530

    chore(checkbox & toggle): update stories for checkbox and toggle switch (#1268)
    ## PR Checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [x] Docs have been added / updated (for bug fixes / features)
    - [ ] If applicable, have a visual design approval
    
    ## PR Type
    
    What kind of change does this PR introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [x] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## What is the current behavior?
    
    <!-- Please describe the current behavior that you are modifying, or
    link to a relevant issue. -->
    
    Issue Number: [CDE-1523](https://jira.eng.vmware.com/browse/CDE-1523)
    
    ## What is the new behavior?
    Refactored checkbox-toggle stories to CSFv3
    
    ## Does this PR introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this PR contains a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    ---------
    
    Signed-off-by: Andrea Fernandes <andrea.fernandes@broadcom.com>
    Co-authored-by: Andrea Fernandes <andreaf1@vmware.com>
    Co-authored-by: GitHub <noreply@github.com>

--- .storybook/stories/checkbox-toggle/checkbox-toggle-container.stories.ts
+++ .storybook/stories/checkbox-toggle/checkbox-toggle-container.stories.ts
@@ -5,37 +5,23 @@
  */
 
 import { ClrCheckboxContainer, ClrCheckboxModule } from '@clr/angular';
-import { Parameters } from '@storybook/addons';
-import { Story } from '@storybook/angular';
+import { moduleMetadata, Story, StoryObj } from '@storybook/angular';
 
-import { setupStorybook } from '../../helpers/setup-storybook.helpers';
+import { getSelectors } from '../../helpers/checkbox-toggle.helpers';
+import { CommonModules } from '../../helpers/common';
 
 enum CheckboxType {
   Checkbox = 'checkbox',
   Toggle = 'toggle',
 }
 
-const defaultStory: Story = args => {
-  const containerSelector = args.type === CheckboxType.Checkbox ? 'clr-checkbox-container' : 'clr-toggle-container';
-  const wrapperSelector = args.type === CheckboxType.Checkbox ? 'clr-checkbox-wrapper' : 'clr-toggle-wrapper';
-  const directive = args.type === CheckboxType.Checkbox ? 'clrCheckbox' : 'clrToggle';
-
-  return {
-    template: `
-      <${containerSelector} [clrInline]="clrInline">
-        <label>{{label}}</label>
-        <${wrapperSelector} *ngFor="let _ of createArray(optionCount); let i = index">
-          <input type="checkbox" ${directive} />
-          <label>Option {{i + 1}}</label>
-        </${wrapperSelector}>
-      </${containerSelector}>
-    `,
-    props: { ...args },
-  };
-};
-
-const defaultParameters: Parameters = {
+export default {
   title: 'Checkbox or Toggle/Checkbox or Toggle Container',
+  decorators: [
+    moduleMetadata({
+      imports: [...CommonModules, ClrCheckboxModule],
+    }),
+  ],
   component: ClrCheckboxContainer,
   argTypes: {
     // inputs
@@ -59,19 +45,94 @@ const defaultParameters: Parameters = {
   },
 };
 
-setupStorybook(ClrCheckboxModule, defaultStory, defaultParameters, generateVariants());
+const CheckboxToggleContainerTemplate: Story = args => {
+  const { containerSelector, wrapperSelector, directive } = getSelectors(args.type);
+  return {
+    template: `
+      <${containerSelector} [clrInline]="clrInline">
+        <label>{{label}}</label>
+        <${wrapperSelector} *ngFor="let _ of createArray(optionCount); let i = index">
+          <input type="checkbox" ${directive} />
+          <label>Option {{i + 1}}</label>
+        </${wrapperSelector}>
+      </${containerSelector}>
+    `,
+    props: args,
+  };
+};
 
-function generateVariants() {
-  const variants: Parameters[] = [];
+const CheckBoxAllTemplate: Story = args => {
+  const { containerSelector, wrapperSelector, directive } = getSelectors(args.type);
+  return {
+    template: `
+      <div style="padding:20px">
+        <span cds-text="subsection">Default</span>
+        <${containerSelector} [clrInline]="false">
+          <label>{{label}}</label>
+          <${wrapperSelector} *ngFor="let _ of createArray(optionCount); let i = index">
+            <input type="checkbox" ${directive} />
+            <label>Option {{i + 1}}</label>
+          </${wrapperSelector}>
+        </${containerSelector}>
+      </div>
+      
+      <div style="padding:20px">
+        <span cds-text="subsection">Inline</span>
+        <${containerSelector} [clrInline]="true">
+          <label>{{label}}</label>
+          <${wrapperSelector} *ngFor="let _ of createArray(optionCount); let i = index">
+            <input type="checkbox" ${directive} />
+            <label>Option {{i + 1}}</label>
+          </${wrapperSelector}>
+        </${containerSelector}>
+      </div>
+      
+      <div style="padding:20px">
+        <span cds-text="subsection">Helper Text</span>
+        <${containerSelector} [clrInline]="false">
+          <label>{{label}}</label>
+          <${wrapperSelector} *ngFor="let _ of createArray(optionCount); let i = index">
+            <input type="checkbox" ${directive} />
+            <label>Option {{i + 1}}</label>
+          </${wrapperSelector}>
+          <clr-control-helper>Helper text</clr-control-helper>
+          <clr-control-error>This field is required!</clr-control-error>
+        </${containerSelector}>
+      </div>
+      
+      <div *ngIf="type==='toggle'" style="padding:20px">
+        <span cds-text="subsection">Right Aligned</span>
+        <${containerSelector} [clrInline]="false" class="clr-toggle-right">
+          <label>{{label}}</label>
+          <${wrapperSelector} *ngFor="let _ of createArray(optionCount); let i = index">
+            <input type="checkbox" ${directive} />
+            <label>Option {{i + 1}}</label>
+          </${wrapperSelector}>
+        </${containerSelector}>
+      </div>
+    `,
+    props: args,
+  };
+};
 
-  for (const type of [CheckboxType.Checkbox, CheckboxType.Toggle]) {
-    for (const clrInline of [false, true]) {
-      variants.push({
-        clrInline,
-        type,
-      });
-    }
-  }
+export const CheckboxOrToggleContainer: StoryObj = {
+  render: CheckboxToggleContainerTemplate,
+};
 
-  return variants;
-}
+export const ShowcaseCheckboxContainer: StoryObj = {
+  render: CheckBoxAllTemplate,
+  args: { type: CheckboxType.Checkbox },
+  parameters: {
+    actions: { disable: true },
+    controls: { disable: true },
+  },
+};
+
+export const ShowcaseToggleContainer: StoryObj = {
+  render: CheckBoxAllTemplate,
+  args: { type: CheckboxType.Toggle },
+  parameters: {
+    actions: { disable: true },
+    controls: { disable: true },
+  },
+};
