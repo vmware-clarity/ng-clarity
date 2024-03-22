ea309de3684f8a2f74f020c76b3909a92e506eb6
Nagaraja <nmarkapuram@vmware.com>
2024-03-22 12:03:34 +0530

    chore: upgraded storybook to 7.6.10 along with csf2 support

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,16 +8,26 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: ['./**/*.stories.{ts,mdx}'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
-  framework: '@storybook/angular',
-  core: {
-    builder: '@storybook/builder-webpack5',
+
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-mdx-gfm'],
+
+  framework: {
+    name: '@storybook/angular',
+    options: {},
   },
+  features: {
+    storyStoreV7: false, // 👈 Opt out of on-demand story loading
+  },
+
   webpackFinal(config) {
     config.resolve = config.resolve || {};
     config.resolve.plugins = config.resolve.plugins || [];
     config.resolve.plugins.push(new TsconfigPathsPlugin({ configFile: './.storybook/tsconfig.json' }));
 
     return config;
+  },
+
+  docs: {
+    autodocs: true,
   },
 };
