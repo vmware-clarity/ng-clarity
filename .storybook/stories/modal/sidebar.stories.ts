adb281eeea5e12a97eb078302c78f637b45e2c61
Ivan Donchev <idonchev@vmware.com>
2024-03-26 20:57:39 +0200

    feat(modal): [POC] sidebar

---
+++ .storybook/stories/modal/sidebar.stories.ts
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2016-2023 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { ClrModal, ClrModalModule, commonStringsDefault } from '@clr/angular';
+import { action } from '@storybook/addon-actions';
+import { moduleMetadata, Story, StoryObj } from '@storybook/angular';
+
+import { CommonModules } from '../../helpers/common';
+
+export default {
+  title: 'Modal/Sidebar',
+  decorators: [
+    moduleMetadata({
+      imports: [...CommonModules, ClrModalModule],
+    }),
+  ],
+  component: ClrModal,
+  argTypes: {
+    // inputs
+    clrModalCloseButtonAriaLabel: { type: 'string', defaultValue: commonStringsDefault.close },
+    clrModalLabelledById: { defaultValue: '' },
+    clrModalStaticBackdrop: { defaultValue: false, control: { type: 'boolean' } },
+    clrModalSize: { defaultValue: 'md', control: { type: 'radio', options: ['sm', 'md', 'lg', 'xl'] } },
+    clrModalSkipAnimation: { defaultValue: false, control: { type: 'boolean' } },
+    clrNoBackdrop: { defaultValue: false, control: { type: 'boolean' } },
+    // outputs
+    clrModalAlternateClose: { control: { disable: true } },
+    clrModalOpenChange: { control: { disable: true } },
+    // methods
+    fadeDone: { control: { disable: true }, table: { disable: true } },
+    open: { control: { disable: true }, table: { disable: true } },
+    close: { control: { disable: true }, table: { disable: true } },
+  },
+  args: {
+    // outputs
+    clrModalAlternateClose: action('clrModalAlternateClose'),
+    clrModalOpenChange: action('clrModalOpenChange'),
+    // story helpers
+    title: 'Sidebar Title',
+    body: 'Hello World!',
+  },
+};
+
+const SidebarTemplate: Story = args => ({
+  template: `
+    <button type="button" class="btn btn-primary" (click)="clrModalOpen = true">Open Sidebar</button>
+    <clr-modal
+      clrSidebar
+      [clrNoBackdrop]="clrNoBackdrop"
+      [clrModalClosable]="clrModalClosable"
+      [clrModalCloseButtonAriaLabel]="clrModalCloseButtonAriaLabel"
+      [clrModalLabelledById]="clrModalLabelledById"
+      [clrModalOpen]="clrModalOpen"
+      [clrModalPreventClose]="clrModalPreventClose"
+      [clrModalSize]="clrModalSize"
+      [clrModalSkipAnimation]="clrModalSkipAnimation"
+      [clrModalStaticBackdrop]="clrModalStaticBackdrop"
+      (clrModalAlternateClose)="clrModalAlternateClose($event)"
+      (clrModalOpenChange)="clrModalOpen = $event; clrModalOpenChange($event)"
+    >
+      <h3 class="modal-title">{{title}}</h3>
+      <div class="modal-body">
+        {{body}}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline" (click)="clrModalOpen = false">Cancel</button>
+        <button type="button" class="btn btn-primary" (click)="clrModalOpen = false">Ok</button>
+      </div>
+    </clr-modal>
+  `,
+  props: args,
+});
+
+export const Sidebar: StoryObj = {
+  render: SidebarTemplate,
+};
+
+export const SidebarSmall: StoryObj = {
+  render: SidebarTemplate,
+  args: {
+    clrModalOpen: false,
+    clrModalSize: 'sm',
+    title: 'Small Sidebar',
+    body: 'This is a small sidebar.',
+  },
+};
+
+export const SidebarMedium: StoryObj = {
+  render: SidebarTemplate,
+  args: {
+    clrModalOpen: false,
+    clrModalSize: 'md',
+    title: 'Medium Sidebar',
+    body: 'This is a medium sidebar.',
+  },
+};
+export const SidebarNoBackdrop: StoryObj = {
+  render: SidebarTemplate,
+  args: {
+    clrModalOpen: false,
+    clrModalSize: 'md',
+    clrNoBackdrop: true,
+    title: 'No Backdrop Sidebar',
+    body: 'This is a medium sidebar without backdrop.',
+  },
+};
+
+export const SidebarLarge: StoryObj = {
+  render: SidebarTemplate,
+  args: {
+    clrModalOpen: false,
+    clrModalSize: 'lg',
+    title: 'Large Sidebar',
+    body: 'This is a large sidebar.',
+  },
+};
+
+export const SidebarExtraLarge: StoryObj = {
+  render: SidebarTemplate,
+  args: {
+    clrModalOpen: false,
+    clrModalSize: 'xl',
+    title: 'Extra-Large Sidebar',
+    body: 'This is a extra-large sidebar.',
+  },
+};
