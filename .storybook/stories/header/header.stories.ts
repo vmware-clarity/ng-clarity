e0345e10d7bc853d5981a997f6808007f4ce354d
Srinivas Padidapelli <padidapellis@padidapell9MD6R.vmware.com>
2024-04-01 16:23:59 +0530

    Revert "chore: visual regression test fix"
    This reverts commit c339c5c2706a3b7fc997ce83d28ae49f0f58be5f.

--- .storybook/stories/header/header.stories.ts
+++ .storybook/stories/header/header.stories.ts
@@ -10,7 +10,7 @@ import { moduleMetadata, StoryFn, StoryObj } from '@storybook/angular';
 import { CommonModules } from '../../helpers/common';
 
 export default {
-  title: 'Header',
+  title: 'Header/Header',
   decorators: [
     moduleMetadata({
       imports: [...CommonModules, ClrMainContainerModule, ClrNavigationModule],
