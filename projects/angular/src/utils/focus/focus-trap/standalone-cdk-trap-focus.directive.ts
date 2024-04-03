42a797b2ac18e117ba1f656013087c341ccea401
Andrea Fernandes <andreaf1@vmware.com>
2024-04-03 11:47:56 +0530

    chore(storybook): fix dependency injection errors for 'ClrStandaloneCdkTrapFocus'

--- projects/angular/src/utils/focus/focus-trap/standalone-cdk-trap-focus.directive.ts
+++ projects/angular/src/utils/focus/focus-trap/standalone-cdk-trap-focus.directive.ts
@@ -4,10 +4,15 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import { CdkTrapFocus } from '@angular/cdk/a11y';
-import { Directive } from '@angular/core';
+import { CdkTrapFocus, FocusTrapFactory } from '@angular/cdk/a11y';
+import { DOCUMENT } from '@angular/common';
+import { Directive, ElementRef, Inject, Optional } from '@angular/core';
 
 @Directive({
   standalone: true,
 })
-export class ClrStandaloneCdkTrapFocus extends CdkTrapFocus {}
+export class ClrStandaloneCdkTrapFocus extends CdkTrapFocus {
+  constructor(elementRef: ElementRef, focusTrapFactory: FocusTrapFactory, @Optional() @Inject(DOCUMENT) document: any) {
+    super(elementRef, focusTrapFactory, document);
+  }
+}
