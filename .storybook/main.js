c08166d63ae70bf9b830f25271598cde1bfb1487
Kevin Buhmann <kbuhmann@vmware.com>
2024-04-11 01:34:20 -0500

    remove blank lines

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,23 +8,19 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: ['./**/*.mdx', './**/*.stories.ts'],
-
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/blocks'],
-
   framework: {
     name: '@storybook/angular',
     options: {},
   },
-
+  docs: {
+    autodocs: true,
+  },
   webpackFinal(config) {
     config.resolve = config.resolve || {};
     config.resolve.plugins = config.resolve.plugins || [];
     config.resolve.plugins.push(new TsconfigPathsPlugin({ configFile: './.storybook/tsconfig.json' }));
 
     return config;
-  },
-
-  docs: {
-    autodocs: true,
   },
 };
