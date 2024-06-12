76b13f825443f175cdc193c5f8850b93fb2c975c
Srinivas Padidapelli <spadidapelli@gmail.com>
2024-06-12 10:28:26 +0530

    fix(a11y): datagrid filter button aria-label fix (#1399)
    ## PR Checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [x] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    - [ ] If applicable, have a visual design approval
    
    ## PR Type
    
    What kind of change does this PR introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    
    - [x] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## What is the current behavior?
    
    <!-- Please describe the current behavior that you are modifying, or
    link to a relevant issue. -->
    
    Issue Number: CDE-1633
    
    ## What is the new behavior?
    
    ## Does this PR introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this PR contains a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information
    
    ---------
    
    Co-authored-by: Srinivas Padidapelli <padidapellis@vmware.com>

--- projects/angular/src/data/datagrid/datagrid-filter.spec.ts
+++ projects/angular/src/data/datagrid/datagrid-filter.spec.ts
@@ -138,9 +138,11 @@ export default function (): void {
         expect(toggle.getAttribute('aria-expanded')).toBe('true');
       });
 
-      it('has a button with the correct aria-label', function () {
+      it('has a button with the correct aria-label', async () => {
+        await context.fixture.whenStable();
+        context.detectChanges();
         const toggle: HTMLButtonElement = context.clarityElement.querySelector('.datagrid-filter-toggle');
-        expect(toggle.getAttribute('aria-label')).toBe('Toggle user filter');
+        expect(toggle.getAttribute('aria-label')).toBe('user filter');
       });
 
       it('has role and label on the filter dialog', function () {
