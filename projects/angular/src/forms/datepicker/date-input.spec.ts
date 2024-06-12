443d168b6d2d2345f7955bd592b63eaba3f99433
Kevin Buhmann <kbuhmann@vmware.com>
2024-06-12 10:32:54 -0500

    chore: use `async`/`await` instead of `.then` (#1433)
    Refactor legacy `.then` syntax to modern `async`/`await` syntax.
    
    CDE-2082
    
    (cherry picked from commit 0edf4bfeebd889984205b20d8f9cbb3fe2a624a3)

--- projects/angular/src/forms/datepicker/date-input.spec.ts
+++ projects/angular/src/forms/datepicker/date-input.spec.ts
@@ -582,30 +582,26 @@ export default function () {
         expect(TestBed.inject(NgControlService).setControl).toHaveBeenCalled();
       }));
 
-      it('marks the form as touched when the markAsTouched event is received', done => {
-        fixture.whenStable().then(() => {
-          const form = fixture.componentInstance.templateForm.form;
-          expect(form.get('date').touched).toBe(false);
+      it('marks the form as touched when the markAsTouched event is received', async () => {
+        await fixture.whenStable();
+        const form = fixture.componentInstance.templateForm.form;
+        expect(form.get('date').touched).toBe(false);
 
-          dateFormControlService.markAsTouched();
+        dateFormControlService.markAsTouched();
 
-          fixture.detectChanges();
-          expect(form.get('date').touched).toBe(true);
-          done();
-        });
+        fixture.detectChanges();
+        expect(form.get('date').touched).toBe(true);
       });
 
-      it('marks the form as dirty when the markAsDirty event is received', done => {
-        fixture.whenStable().then(() => {
-          const form = fixture.componentInstance.templateForm.form;
-          expect(form.get('date').dirty).toBe(false);
+      it('marks the form as dirty when the markAsDirty event is received', async () => {
+        await fixture.whenStable();
+        const form = fixture.componentInstance.templateForm.form;
+        expect(form.get('date').dirty).toBe(false);
 
-          dateFormControlService.markAsDirty();
+        dateFormControlService.markAsDirty();
 
-          fixture.detectChanges();
-          expect(form.get('date').dirty).toBe(true);
-          done();
-        });
+        fixture.detectChanges();
+        expect(form.get('date').dirty).toBe(true);
       });
 
       it('outputs the date when the user manually changes the date', () => {
