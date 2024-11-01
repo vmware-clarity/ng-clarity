df200a4bc8332a9a0b55d2dce92866ea66b6f53a
mivaylo <mivaylo@vmware.com>
2024-11-01 15:44:58 +0200

    feat(storybook): add use cases for nested components

--- .storybook/stories/accordion/nested-accordion.stories.ts
+++ .storybook/stories/accordion/nested-accordion.stories.ts
@@ -52,8 +52,58 @@ const template = `
   </clr-accordion>
 `;
 
+const nestedComponentsTemplate = `
+  <clr-accordion>
+    <clr-accordion-panel>
+      <clr-accordion-title>Parent Title {{ i + 1 }}</clr-accordion-title>
+      <clr-accordion-content>
+        TEST
+        <!--        <clr-accordion>-->
+        <!--          <clr-alert>Alert</clr-alert>-->
+        <!--          <clr-badge></clr-badge>-->
+        <!--          <clr-button>Alert</clr-button>-->
+        <!--          <clr-button-group>Alert</clr-button-group>-->
+        <!--          <clr-card>Alert</clr-card>-->
+        <!--          <clr-checkbox-wrapper>Alert</clr-checkbox-wrapper>-->
+        <!--          <clr-combobox>Alert</clr-combobox>-->
+        <!--          <clr-datagrid>Alert</clr-datagrid>-->
+        <!--          <clr-datalist-container>Alert</clr-datalist-container>-->
+        <!--          <clr-datapicker>Alert</clr-datapicker>-->
+        <!--          <clr-dropdown>Alert</clr-dropdown>-->
+        <!--          <clr-file-input-container>Alert</clr-file-input-container>-->
+        <!--          <clr-icon>Alert</clr-icon>-->
+        <!--          <clr-input-container>Alert</clr-input-container>-->
+        <!--          <clr-label>Alert</clr-label>-->
+        <!--          <clr-list>Alert</clr-list>-->
+        <!--          <clr-modal>Alert</clr-modal>-->
+        <!--          <clr-password-container>Alert</clr-password-container>-->
+        <!--          <clr-progress-bar>Alert</clr-progress-bar>-->
+        <!--          <clr-radio-container>Alert</clr-radio-container>-->
+        <!--          <clr-range-containerAlert</clr-range-container>-->
+        <!--          <clr-select-container>Alert</clr-select-container>-->
+        <!--          <clr-signpost>Alert</clr-radio-container>-->
+        <!--          <clr-spinner>Alert</clr-radio-container>-->
+        <!--          <clr-table>Alert</clr-radio-container>-->
+        <!--          <clr-textarea-container>Alert</clr-radio-container>-->
+        <!--          <clr-timeline>Alert</clr-radio-container>-->
+        <!--          <clr-toggle-wrapper>Alert</clr-radio-container>-->
+        <!--          <clr-tooltip>Alert</clr-radio-container>-->
+        <!--          <clr-timeline>Alert</clr-radio-container>-->
+        <!--          <clr-tree>Alert</clr-radio-container>-->
+        <!--          <clr-wizard>Alert</clr-radio-container>-->
+        <!--        </clr-accordion>-->
+      </clr-accordion-content>
+    </clr-accordion-panel>
+  </clr-accordion>
+`;
+
 const NestedAccordionTemplate: StoryFn = args => ({
   template,
+  props: args,
+});
+
+const NestedComponentsAccordionTemplate: StoryFn = args => ({
+  nestedComponentsTemplate,
   props: args,
 });
 
@@ -67,4 +117,8 @@ export const Opened: StoryObj = {
     openIndex: 0,
     nestedOpenIndex: 0,
   },
+};
+
+export const Nstd: StoryObj = {
+  render: NestedComponentsAccordionTemplate,
 };
