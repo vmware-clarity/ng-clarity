15e59e717dfb757d3625c97527abc0844a40a142
Daniel Tsanev <tsanevd@vmware.com>
2024-12-02 11:12:47 +0200

    fix(tabs): export tabs actions position type

--- projects/angular/src/layout/tabs/index.ts
+++ projects/angular/src/layout/tabs/index.ts
@@ -14,5 +14,6 @@ export * from './tab-link.directive';
 export * from './tab-action.directive';
 export * from './tabs.module';
 
+export { ClrTabsActionsPosition } from './tabs-actions';
 export { TabsWillyWonka as ÇlrTabsWillyWonka } from './chocolate/tabs-willy-wonka';
 export { ActiveOompaLoompa as ÇlrActiveOompaLoompa } from './chocolate/active-oompa-loompa';
