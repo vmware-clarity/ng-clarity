1223e77613a795cabfcab5c4116ed5d3366ae69c
Valentin Mladenov <mvalentin@vmware.com>
2024-12-03 14:01:40 +0200

    fix(accordion): add aria heading level

--- projects/angular/src/accordion/accordion-title.ts
+++ projects/angular/src/accordion/accordion-title.ts
@@ -5,12 +5,21 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'clr-accordion-title, clr-step-title',
   template: `<ng-content></ng-content>`,
-  host: { '[class.clr-accordion-title]': 'true', role: 'heading' },
+  host: {
+    '[class.clr-accordion-title]': 'true',
+    '[attr.aria-level]': 'ariaLevel',
+    role: 'heading',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ClrAccordionTitle {}
+export class ClrAccordionTitle {
+  /**
+   * Level of the accordion/stepper heading from 1 to 7. Default is 3.
+   */
+  @Input('clrAccordionLevel') ariaLevel = 3;
+}
