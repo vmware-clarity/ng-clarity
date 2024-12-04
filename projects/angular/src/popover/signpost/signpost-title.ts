cbfdd077be3b1ce1a61abca3a5132c22e0dc0d54
Srinivas Padidapelli <padidapellis@vmware.com>
2024-12-04 14:31:20 +0530

    feat(signpost): signpost title enhancement

--- projects/angular/src/popover/signpost/signpost-title.ts
+++ projects/angular/src/popover/signpost/signpost-title.ts
@@ -10,6 +10,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'clr-signpost-title',
   template: `<ng-content></ng-content>`,
-  host: { '[class.clr-Signpost-title]': 'true' },
+  host: { '[class.clr-signpost-title]': 'true' },
 })
 export class ClrSignpostTitle {}
