b4de494255356dc4efa67c6e3d4942b1cdcda76b
Ivan Donchev <idonchev@vmware.com>
2025-01-08 10:51:43 +0200

    fix: remove obsolete and unstable tests

--- projects/angular/src/forms/styles/containers.spec.ts
+++ projects/angular/src/forms/styles/containers.spec.ts
@@ -11,7 +11,7 @@ import { ClrIconModule } from '../../icon/icon.module';
 
 @Component({
   template: `
-    <form class="clr-form clr-form-{{ layout }}" style="width: 2000px">
+    <form class="clr-form clr-form-{{ layout }}">
       <div class="clr-form-control" [ngClass]="{ 'clr-row': grid }" id="text">
         <label
           for="{{ layout }}-basic"
@@ -983,7 +983,12 @@ describe('Form layouts', () => {
     horizontalTests();
   });
 
-  describe('Horizontal with Grid', () => {
+  // Disabling for v15 only as part of this unrelated backport:
+  // https://github.com/vmware-clarity/ng-clarity/pull/1658
+  // It causes random failures dependent on the browser/os and is currently considered
+  // unfeasible to fix, as this is covered by visual tests.
+  // So far it remains in newer version with the option to remove there too, if failing.
+  xdescribe('Horizontal with Grid', () => {
     beforeEach(() => {
       instance.layout = 'horizontal';
       instance.grid = true;
@@ -1000,7 +1005,12 @@ describe('Form layouts', () => {
     compactTests();
   });
 
-  describe('Compact with Grid', () => {
+  // Disabling for v15 only as part of this unrelated backport:
+  // https://github.com/vmware-clarity/ng-clarity/pull/1658
+  // It causes random failures dependent on the browser/os and is currently considered
+  // unfeasible to fix, as this is covered by visual tests.
+  // So far it remains in newer version with the option to remove there too, if failing.
+  xdescribe('Compact with Grid', () => {
     beforeEach(() => {
       instance.layout = 'compact';
       instance.grid = true;
