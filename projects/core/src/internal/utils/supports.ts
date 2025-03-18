171cf9622150a63888d2616440f8706aa7bf75d4
Ivan Donchev <idonchev@vmware.com>
2025-03-18 11:13:32 +0200

    chore: merge cds-core tools to clr-angular

---
+++ projects/core/src/internal/utils/supports.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016-2023 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { camelCaseToKebabCase } from './string.js';
+
+export interface FeatureSupportMatrix {
+  js?: boolean;
+}
+
+class BrowserFeatures {
+  supports = {
+    js: true,
+  };
+
+  constructor() {
+    if (!document.body.hasAttribute('cds-supports') || document.body.getAttribute('cds-supports') === 'no-js') {
+      const supports = camelCaseToKebabCase(
+        Object.keys(this.supports).reduce(
+          (prev, next) => `${prev} ${(this.supports as any)[next] ? next : `no-${next}`}`,
+          ''
+        )
+      ).trim();
+      document.body.setAttribute('cds-supports', supports);
+    }
+  }
+}
+
+export const browserFeatures = new BrowserFeatures();
