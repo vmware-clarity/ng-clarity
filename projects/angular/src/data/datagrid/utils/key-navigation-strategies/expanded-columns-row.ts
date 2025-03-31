13bd685532d46f5c71f628be913da941f5c14320
Valentin Mladenov <mvalentin@vmware.com>
2025-03-31 15:10:17 +0300

    fix(datagrid): column page up and page down keys navigation completed

--- projects/angular/src/data/datagrid/utils/key-navigation-strategies/expanded-columns-row.ts
+++ projects/angular/src/data/datagrid/utils/key-navigation-strategies/expanded-columns-row.ts
@@ -177,15 +177,14 @@ export class ClrExpandedColumnsRowKeyNavigationStrategy extends ClrDefaultKeyNav
     if (!this.isActionCell(currentCellCoords)) {
       if (this.isRowReplaced(nextCellCoords.y)) {
         nextCellCoords.y = nextCellCoords.y + 1;
-
-        if (this.isDetailsRow(nextCellCoords.y)) {
-          nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
-        }
+        nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
       } else if (this.isDetailsRow(currentCellCoords.y) && !this.isDetailsRow(nextCellCoords.y)) {
         nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
       } else if (this.isDetailsRow(nextCellCoords.y)) {
         nextCellCoords.x = currentCellCoords.x - this.actionCellCount(currentCellCoords.y);
       }
+    } else if (this.isDetailsRow(nextCellCoords.y)) {
+      nextCellCoords.y = nextCellCoords.y - 1;
     }
 
     return nextCellCoords;
@@ -198,34 +197,26 @@ export class ClrExpandedColumnsRowKeyNavigationStrategy extends ClrDefaultKeyNav
 
     const numOfRows = this.rows ? this.rows.length - 1 : 0;
     const itemsPerPage = this.getItemsPerPage();
-    const numOfColumns = numOfRows ? this.getCellsForRow(0).length - 1 : 0;
 
     nextCellCoords.y = currentCellCoords.y + itemsPerPage >= numOfRows ? numOfRows : currentCellCoords.y + itemsPerPage;
 
-    // const isActionCell = this.isActionCell(currentCellCoords);
+    if (!this.isActionCell(currentCellCoords)) {
+      if (this.isRowReplaced(nextCellCoords.y)) {
+        if (nextCellCoords.y < numOfRows) {
+          nextCellCoords.y = nextCellCoords.y + 1;
+        }
 
-    if (this.isActionCell(currentCellCoords) || this.isRowReplaced(nextCellCoords.y)) {
+        if (this.isDetailsRow(nextCellCoords.y)) {
+          nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
+        }
+      } else if (this.isDetailsRow(currentCellCoords.y) && !this.isDetailsRow(nextCellCoords.y)) {
+        nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
+      } else if (this.isDetailsRow(nextCellCoords.y)) {
+        nextCellCoords.x = currentCellCoords.x - this.actionCellCount(currentCellCoords.y);
+      }
+    } else if (this.isDetailsRow(nextCellCoords.y)) {
       nextCellCoords.y = nextCellCoords.y - 1;
-    } else if (this.getCellsForRow(currentCellCoords.y).length > numOfColumns) {
-      nextCellCoords.x = currentCellCoords.x - this.actionCellCount(currentCellCoords.y);
-    } else {
-      nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
     }
-
-    // if (this.isActionCell(currentCellCoords) && this.isDetailsRow(nextCellCoords.y)) {
-    //   console.log(1)
-    //   nextCellCoords.y = nextCellCoords.y - 1;
-    // } else if (this.isDetailsRow(nextCellCoords.y)) {
-    //   console.log(2)
-    //   nextCellCoords.x = nextCellCoords.x - this.actionCellCount(currentCellCoords.y);
-    // } else if (this.isRowReplaced(nextCellCoords.y)) {
-    //   console.log(3)
-    //   nextCellCoords.y = nextCellCoords.y + 1;
-    //   nextCellCoords.x = nextCellCoords.x - this.actionCellCount(currentCellCoords.y);
-    // } else {
-    //   console.log(4)
-    //   nextCellCoords.x = nextCellCoords.x + this.actionCellCount(nextCellCoords.y);
-    // }
 
     return nextCellCoords;
   }
