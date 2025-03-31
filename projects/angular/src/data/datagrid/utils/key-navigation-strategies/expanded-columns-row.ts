7a4819765dfc5274ff111e842ce17c8ae22ab883
Valentin Mladenov <mvalentin@vmware.com>
2025-03-31 13:32:09 +0300

    fix(datagrid): column page up key navigation completed

--- projects/angular/src/data/datagrid/utils/key-navigation-strategies/expanded-columns-row.ts
+++ projects/angular/src/data/datagrid/utils/key-navigation-strategies/expanded-columns-row.ts
@@ -51,7 +51,7 @@ export class ClrExpandedColumnsRowKeyNavigationStrategy extends ClrDefaultKeyNav
   }
 
   keyDown(currentCellCoords: CellCoordinates) {
-    console.log('keyDown ClrExpandedDetailsRowKeyNavigationStrategy');
+    console.log('keyDown ClrExpandedColumnsRowKeyNavigationStrategy');
 
     const nextCellCoords = this.createNextCellCoords(currentCellCoords);
 
@@ -77,7 +77,7 @@ export class ClrExpandedColumnsRowKeyNavigationStrategy extends ClrDefaultKeyNav
   }
 
   keyLeft(currentCellCoords: CellCoordinates) {
-    console.log('keyLeft ClrExpandedDetailsRowKeyNavigationStrategy');
+    console.log('keyLeft ClrExpandedColumnsRowKeyNavigationStrategy');
 
     const nextCellCoords = this.createNextCellCoords(currentCellCoords);
 
@@ -96,7 +96,7 @@ export class ClrExpandedColumnsRowKeyNavigationStrategy extends ClrDefaultKeyNav
   }
 
   keyRight(currentCellCoords: CellCoordinates) {
-    console.log('keyRight ClrExpandedDetailsRowKeyNavigationStrategy');
+    console.log('keyRight ClrExpandedColumnsRowKeyNavigationStrategy');
 
     const nextCellCoords = this.createNextCellCoords(currentCellCoords);
 
@@ -127,7 +127,7 @@ export class ClrExpandedColumnsRowKeyNavigationStrategy extends ClrDefaultKeyNav
   }
 
   keyEnd(currentCellCoords: CellCoordinates, ctrlKey: boolean) {
-    console.log('keyEnd ClrExpandedDetailsRowKeyNavigationStrategy');
+    console.log('keyEnd ClrExpandedColumnsRowKeyNavigationStrategy');
 
     const nextCellCoords = this.createNextCellCoords(currentCellCoords);
 
@@ -148,7 +148,7 @@ export class ClrExpandedColumnsRowKeyNavigationStrategy extends ClrDefaultKeyNav
   }
 
   keyHome(currentCellCoords: CellCoordinates, ctrlKey: boolean) {
-    console.log('keyHome ClrExpandedDetailsRowKeyNavigationStrategy');
+    console.log('keyHome ClrExpandedColumnsRowKeyNavigationStrategy');
 
     const nextCellCoords = this.createNextCellCoords(currentCellCoords);
 
@@ -167,36 +167,65 @@ export class ClrExpandedColumnsRowKeyNavigationStrategy extends ClrDefaultKeyNav
   }
 
   keyPageUp(currentCellCoords: CellCoordinates) {
-    console.log('keyPageUp ClrExpandedDetailsRowKeyNavigationStrategy');
+    console.log('keyPageUp ClrExpandedColumnsRowKeyNavigationStrategy');
 
     const nextCellCoords = this.createNextCellCoords(currentCellCoords);
     const itemsPerPage = this.getItemsPerPage();
 
     nextCellCoords.y = currentCellCoords.y - itemsPerPage > 0 ? currentCellCoords.y - itemsPerPage + 1 : 1;
 
-    const isActionCell = this.isActionCell(currentCellCoords);
+    if (!this.isActionCell(currentCellCoords)) {
+      if (this.isRowReplaced(nextCellCoords.y)) {
+        nextCellCoords.y = nextCellCoords.y + 1;
 
-    if (isActionCell && this.isDetailsRow(nextCellCoords.y)) {
-      nextCellCoords.y = nextCellCoords.y - 1;
-    } else if (this.isRowReplaced(nextCellCoords.y)) {
-      nextCellCoords.y = nextCellCoords.y - 1;
-
-      if (!this.isDetailsRow(nextCellCoords.y)) {
+        if (this.isDetailsRow(nextCellCoords.y)) {
+          nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
+        }
+      } else if (this.isDetailsRow(currentCellCoords.y) && !this.isDetailsRow(nextCellCoords.y)) {
         nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
+      } else if (this.isDetailsRow(nextCellCoords.y)) {
+        nextCellCoords.x = currentCellCoords.x - this.actionCellCount(currentCellCoords.y);
       }
-    } else if (this.isDetailsRow(currentCellCoords.y) && !this.isDetailsRow(nextCellCoords.y)) {
-      nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
-    } else if (!isActionCell && this.isDetailsRow(nextCellCoords.y)) {
-      nextCellCoords.x = currentCellCoords.x - this.actionCellCount(currentCellCoords.y);
     }
 
     return nextCellCoords;
   }
 
   keyPageDown(currentCellCoords: CellCoordinates) {
-    console.log('keyPageDown ClrExpandedDetailsRowKeyNavigationStrategy');
+    console.log('keyPageDown ClrExpandedColumnsRowKeyNavigationStrategy');
 
-    const nextCellCoords = super.keyPageDown(currentCellCoords);
+    const nextCellCoords = this.createNextCellCoords(currentCellCoords);
+
+    const numOfRows = this.rows ? this.rows.length - 1 : 0;
+    const itemsPerPage = this.getItemsPerPage();
+    const numOfColumns = numOfRows ? this.getCellsForRow(0).length - 1 : 0;
+
+    nextCellCoords.y = currentCellCoords.y + itemsPerPage >= numOfRows ? numOfRows : currentCellCoords.y + itemsPerPage;
+
+    // const isActionCell = this.isActionCell(currentCellCoords);
+
+    if (this.isActionCell(currentCellCoords) || this.isRowReplaced(nextCellCoords.y)) {
+      nextCellCoords.y = nextCellCoords.y - 1;
+    } else if (this.getCellsForRow(currentCellCoords.y).length > numOfColumns) {
+      nextCellCoords.x = currentCellCoords.x - this.actionCellCount(currentCellCoords.y);
+    } else {
+      nextCellCoords.x = currentCellCoords.x + this.actionCellCount(nextCellCoords.y);
+    }
+
+    // if (this.isActionCell(currentCellCoords) && this.isDetailsRow(nextCellCoords.y)) {
+    //   console.log(1)
+    //   nextCellCoords.y = nextCellCoords.y - 1;
+    // } else if (this.isDetailsRow(nextCellCoords.y)) {
+    //   console.log(2)
+    //   nextCellCoords.x = nextCellCoords.x - this.actionCellCount(currentCellCoords.y);
+    // } else if (this.isRowReplaced(nextCellCoords.y)) {
+    //   console.log(3)
+    //   nextCellCoords.y = nextCellCoords.y + 1;
+    //   nextCellCoords.x = nextCellCoords.x - this.actionCellCount(currentCellCoords.y);
+    // } else {
+    //   console.log(4)
+    //   nextCellCoords.x = nextCellCoords.x + this.actionCellCount(nextCellCoords.y);
+    // }
 
     return nextCellCoords;
   }
