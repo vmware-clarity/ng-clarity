0966bcddf9b6ec1151a918376705c96880cc2990
Delchina Angelova <delchina.angelova@broadcom.com>
2025-04-30 13:34:58 +0300

    Refactor Stories for Label

---
+++ .storybook/stories/label/label.storybook.component.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016-2025 Broadcom. All Rights Reserved.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'storybook-label',
+  template: `
+    <ng-container *ngIf="clickable">
+      <a href="javascript://" class="label clickable" *ngFor="let type of labelColorTypes" [class]="type">
+        <span class="text">{{ content }}</span>
+        <span *ngIf="badgeContent" class="badge">{{ badgeContent }}</span>
+      </a>
+    </ng-container>
+    <ng-container *ngIf="!clickable">
+      <span class="label" *ngFor="let type of labelColorTypes" [class]="type">
+        <span class="text">{{ content }}</span>
+        <span *ngIf="badgeContent" class="badge">{{ badgeContent }}</span>
+        <cds-icon *ngIf="closeIcon" shape="close"></cds-icon>
+      </span>
+    </ng-container>
+  `,
+})
+export class LabelStoryBookComponent {
+  @Input() badgeContent = '';
+  @Input() content = 'Hello World!';
+  @Input() clickable = false;
+  @Input() closeIcon = false;
+  @Input() labelColorTypes = [''];
+}
