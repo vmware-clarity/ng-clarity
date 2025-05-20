2fe09f81ff8342f8e3db0c81fa544a15a3634e07
Daniel Tsanev <127101685+dtsanevmw@users.noreply.github.com>
2025-05-20 14:15:43 +0300

    feat(datagrid): use expandable animation directive instead component (#1827)
    ## PR Checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    - [ ] If applicable, have a visual design approval
    
    ## PR Type
    
    What kind of change does this PR introduce?
    
    Simplifying datagrid-row template and allow what we are trying to
    implement in #1791 which is currently not possible due to the multiple
    content projections inside datagrid-row that are creating issue between
    Calculate and Display modes for datagrid.
    
    <!-- Please check the one that applies to this PR using "x". -->
    
    - [ ] Bugfix
    - [X] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## What is the current behavior?
    
    <!-- Please describe the current behavior that you are modifying, or
    link to a relevant issue. -->
    
    Issue Number: N/A
    
    ## What is the new behavior?
    
    ## Does this PR introduce a breaking change?
    
    - [ ] Yes
    - [X] No
    
    <!-- If this PR contains a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    ## Other information

--- projects/angular/src/utils/animations/expandable-animation/index.ts
+++ projects/angular/src/utils/animations/expandable-animation/index.ts
@@ -8,7 +8,9 @@
 import { Type } from '@angular/core';
 
 import { ClrExpandableAnimation } from './expandable-animation';
+import { ClrExpandableAnimationDirective } from './expandable-animation.directive';
 
 export * from './expandable-animation';
+export * from './base-expandable-animation';
 
-export const EXPANDABLE_ANIMATION_DIRECTIVES: Type<any>[] = [ClrExpandableAnimation];
+export const EXPANDABLE_ANIMATION_DIRECTIVES: Type<any>[] = [ClrExpandableAnimation, ClrExpandableAnimationDirective];
