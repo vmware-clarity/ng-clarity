fcf85370c87eb50d36ae7acb160097a8fd3b9871
Kevin Buhmann <kevin.buhmann@broadcom.com>
2025-06-02 08:21:26 -0500

    refactor demo pages

--- projects/demo/src/app/wizard/wizard-basic.demo.ts
+++ projects/demo/src/app/wizard/wizard-basic.demo.ts
@@ -8,6 +8,12 @@
 import { Component, ViewChild } from '@angular/core';
 import { ClrWizard } from '@clr/angular';
 
+const defaultPageNumbers = [1, 2, 3];
+
+const scrollingPageNumbers = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+];
+
 @Component({
   selector: 'clr-wizard-basic',
   templateUrl: './wizard-basic.demo.html',
@@ -17,4 +23,8 @@ export class WizardBasicDemo {
   open = false;
   fullScreen = false;
   scrollingSteps = false;
+
+  get pageNumbers() {
+    return this.scrollingSteps ? scrollingPageNumbers : defaultPageNumbers;
+  }
 }
