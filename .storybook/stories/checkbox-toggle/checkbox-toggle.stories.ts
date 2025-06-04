aa2f0c3b225ec10f4d56095c6b60a1292b3f075f
Valentin Mladenov <mvalentin@vmware.com>
2025-06-04 17:58:00 +0300

    feat(checkbox, radio): remove border radius and add long label stories

--- .storybook/stories/checkbox-toggle/checkbox-toggle.stories.ts
+++ .storybook/stories/checkbox-toggle/checkbox-toggle.stories.ts
@@ -50,6 +50,24 @@ export const CheckboxOrToggle: StoryObj = {
   },
 };
 
+export const CheckboxLongLabel: StoryObj = {
+  args: {
+    type: CheckboxType.Checkbox,
+    templateMode: 'single',
+    label:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+};
+
+export const ToggleLongLabel: StoryObj = {
+  args: {
+    type: CheckboxType.Toggle,
+    templateMode: 'single',
+    label:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+};
+
 export const ShowcaseCheckbox: StoryObj = {
   args: {
     type: CheckboxType.Checkbox,
