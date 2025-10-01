b6e038fc9a7a1a883e69b8c5aff9b9a10841c161
dtsanevmw <daniel.tsanev@broadcom.com>
2025-10-01 14:45:45 +0300

    fix: remove first active condition

--- projects/angular/src/forms/combobox/providers/combobox-focus-handler.service.ts
+++ projects/angular/src/forms/combobox/providers/combobox-focus-handler.service.ts
@@ -73,9 +73,6 @@ export class ComboboxFocusHandler<T> {
   }
 
   focusFirstActive() {
-    if (this.selectionService.editable && !this.selectionService.multiselectable) {
-      return;
-    }
     if (this.optionData.length > 0) {
       if (this.selectionService.selectionModel.isEmpty()) {
         this.pseudoFocus.select(this.optionData[0]);
