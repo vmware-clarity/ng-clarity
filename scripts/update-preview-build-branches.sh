c5f3f83ef9f6bd33e72ac8987294bba0786c53c8
dtsanevmw <daniel.tsanev@broadcom.com>
2025-10-08 15:27:45 +0300

    chore: update _build:ui copy scripts and preview branches

--- scripts/update-preview-build-branches.sh
+++ scripts/update-preview-build-branches.sh
@@ -18,16 +18,29 @@ rm -rf ./preview-build-clr-angular/*
 cp -r ./dist/clr-ui/* ./preview-build-clr-ui
 cp -r ./dist/clr-angular/* ./preview-build-clr-angular
 
+shortsha=$(echo "${GIT_COMMIT_SHA}" | cut -c1-7)
+next_version="0.0.0-preview.${shortsha}"
+
+if [ ! -f package.json ]; then
+  echo "ERROR: ${dir}/package.json not found. Ensure your dist includes package.json." >&2
+fi
+
+# Write version into package.json (no tags)
+npm version "${next_version}" --no-git-tag-version --allow-same-version
+
+# Log version
+echo "Preview version for ${dir}: ${next_version}"
+
 # push @clr/ui
 pushd ./preview-build-clr-ui
 git add .
-git commit -m "update build for commit $GIT_COMMIT_SHA"
+git commit -m "build: preview build (${next_version}) for ${GIT_COMMIT_SHA}"
 git push origin preview-build-clr-ui:refs/heads/preview-build/$GIT_BRANCH/clr-ui
 popd
 
 # push @clr/angular
 pushd ./preview-build-clr-angular
 git add .
-git commit -m "update build for commit $GIT_COMMIT_SHA"
+git commit -m "build: preview build (${next_version}) for ${GIT_COMMIT_SHA}"
 git push origin preview-build-clr-angular:refs/heads/preview-build/$GIT_BRANCH/clr-angular
 popd
