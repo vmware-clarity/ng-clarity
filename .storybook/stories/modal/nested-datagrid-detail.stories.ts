7b70723844bba1f4e6dbbf834d29d8790fb81202
Valentin Mladenov <valentin.mladenov@broadcom.com>
2025-10-20 15:59:16 +0300

    fix(datagrid): single action overflow popup position (#2076)
    ## PR Checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    - [ ] If applicable, have a visual design approval
    
    ## PR Type
    
    What kind of change does this PR introduce?
    
    - [x] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## What is the current behavior?
    
    Issue Number: CDE-2984
    
    ## What is the new behavior?
    
    ## Does this PR introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    ## Other information
    
    ---------
    
    Co-authored-by: Valentin Mladenov <mvalentin@vmware.com>
    Co-authored-by: GitHub <noreply@github.com>
    (cherry picked from commit eb88c699ac01b7854d858eaa8241c4d64786f07b)

--- .storybook/stories/modal/nested-datagrid-detail.stories.ts
+++ .storybook/stories/modal/nested-datagrid-detail.stories.ts
@@ -25,6 +25,8 @@ export default {
   args: {
     // story helpers
     elements,
+    showActionOverflow: false,
+    modalOpen: false,
   },
 };
 
@@ -40,54 +42,73 @@ const NestedDatagridTemplate: StoryFn = args => ({
         }
       }
     </style>
-    <div><strong>This story is NOT an endorsement of this UX pattern.</strong></div>
+    <div [style.min-height.px]="showActionOverflow ? 650 : 0">
+      <div><strong>This story is NOT an endorsement of this UX pattern.</strong></div>
 
-    <button type="button" class="btn btn-primary" (click)="modalOpen = true">Open Modal</button>
+      <button type="button" class="btn btn-primary" (click)="modalOpen = true">Open Modal</button>
 
-    <clr-modal [(clrModalOpen)]="modalOpen">
-      <h3 class="modal-title">Modal</h3>
-      <div class="modal-body">
-        <clr-datagrid *ngIf="modalOpen">
-          <clr-dg-column [style.width.px]="250">Name</clr-dg-column>
-          <clr-dg-column [style.width.px]="250">Symbol</clr-dg-column>
-          <clr-dg-column [style.width.px]="250">Number</clr-dg-column>
-          <clr-dg-column>Electronegativity</clr-dg-column>
+      <clr-modal [(clrModalOpen)]="modalOpen">
+        <h3 class="modal-title">Modal</h3>
+        <div class="modal-body">
+          <clr-datagrid *ngIf="modalOpen">
+            <clr-dg-column [style.width.px]="250">Name</clr-dg-column>
+            <clr-dg-column [style.width.px]="250">Symbol</clr-dg-column>
+            <clr-dg-column [style.width.px]="250">Number</clr-dg-column>
+            <clr-dg-column>Electronegativity</clr-dg-column>
 
-          <clr-dg-row *clrDgItems="let element of elements; let index = index" [clrDgItem]="element">
-            <clr-dg-cell>{{ element.name }}</clr-dg-cell>
-            <clr-dg-cell>{{ element.symbol }}</clr-dg-cell>
-            <clr-dg-cell>{{ element.number }}</clr-dg-cell>
-            <clr-dg-cell class="electronegativity-container">
-              {{ element.electronegativity }}
-              <div [style.width.%]="(element.electronegativity * 100) / 5" class="electronegativity-bar">&nbsp;</div>
-            </clr-dg-cell>
-          </clr-dg-row>
+            <clr-dg-row *clrDgItems="let element of elements; let index = index" [clrDgItem]="element">
+              <clr-dg-action-overflow *ngIf="showActionOverflow">
+                <button class="action-item">Edit</button>
+                <button class="action-item">Delete</button>
+              </clr-dg-action-overflow>
+              <clr-dg-cell>{{ element.name }}</clr-dg-cell>
+              <clr-dg-cell>{{ element.symbol }}</clr-dg-cell>
+              <clr-dg-cell>{{ element.number }}</clr-dg-cell>
+              <clr-dg-cell class="electronegativity-container">
+                {{ element.electronegativity }}
+                <div [style.width.%]="(element.electronegativity * 100) / 5" class="electronegativity-bar">&nbsp;</div>
+              </clr-dg-cell>
+            </clr-dg-row>
 
-          <clr-dg-detail *clrIfDetail="let element">
-            <clr-dg-detail-header>{{ element.name }}</clr-dg-detail-header>
-            <clr-dg-detail-body>
-              Pressing escape should only this detail pane, not the modal.
-              <br />
-              {{ element | json }}
-            </clr-dg-detail-body>
-          </clr-dg-detail>
+            <clr-dg-detail *clrIfDetail="let element">
+              <clr-dg-detail-header>{{ element.name }}</clr-dg-detail-header>
+              <clr-dg-detail-body>
+                Pressing escape should only this detail pane, not the modal.
+                <br />
+                {{ element | json }}
+              </clr-dg-detail-body>
+            </clr-dg-detail>
 
-          <clr-dg-footer>
-            <clr-dg-pagination #pagination>
-              <clr-dg-page-size [clrPageSizeOptions]="[10, 20, 50, 100]">Elements per page</clr-dg-page-size>
-              {{ pagination.firstItem + 1 }} - {{ pagination.lastItem + 1 }} of {{ pagination.totalItems }} elements
-            </clr-dg-pagination>
-          </clr-dg-footer>
-        </clr-datagrid>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" (click)="modalOpen = false">Close</button>
-      </div>
-    </clr-modal>
+            <clr-dg-footer>
+              <clr-dg-pagination #pagination>
+                <clr-dg-page-size [clrPageSizeOptions]="[10, 20, 50, 100]">Elements per page</clr-dg-page-size>
+                {{ pagination.firstItem + 1 }} - {{ pagination.lastItem + 1 }} of {{ pagination.totalItems }} elements
+              </clr-dg-pagination>
+            </clr-dg-footer>
+          </clr-datagrid>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" (click)="modalOpen = false">Close</button>
+        </div>
+      </clr-modal>
+    </div>
   `,
   props: args,
 });
 
 export const NestedDatagridDetail: StoryObj = {
   render: NestedDatagridTemplate,
+};
+
+export const NestedDatagridWithOpenedActions: StoryObj = {
+  render: NestedDatagridTemplate,
+  args: {
+    modalOpen: true,
+    showActionOverflow: true,
+  },
+  play({ canvasElement }) {
+    setTimeout(() => {
+      (canvasElement.querySelector('button.datagrid-action-toggle') as HTMLElement).click();
+    }, 300);
+  },
 };
