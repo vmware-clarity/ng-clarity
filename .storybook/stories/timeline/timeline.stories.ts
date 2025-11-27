f505572d803102a9aa10cbf179c5416ac2d1ffab
Valentin Mladenov <valentin.mladenov@broadcom.com>
2025-11-27 13:03:47 +0200

    fix(timeline): remove state icons styles from content inside body (#2114) (backport to main) (#2116)
    Backport of  #2114 (17425847e)
    ## PR Checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [x] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    - [ ] If applicable, have a visual design approval
    
    ## PR Type
    
    What kind of change does this PR introduce?
    
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## What is the current behavior?
    Timeline Header icon styles spill over timeline body icons.
    
    Issue Number: CDE-2890
    
    ## What is the new behavior?
    Timeline Header icon styles will work only for them.
    
    ## Does this PR introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    ## Other information
    
    ---------
    
    Co-authored-by: Valentin Mladenov <mvalentin@vmware.com>
    Co-authored-by: GitHub <noreply@github.com>
    
    ---------
    
    Co-authored-by: Valentin Mladenov <mvalentin@vmware.com>
    Co-authored-by: GitHub <noreply@github.com>

--- .storybook/stories/timeline/timeline.stories.ts
+++ .storybook/stories/timeline/timeline.stories.ts
@@ -27,6 +27,7 @@ export default {
   args: {
     // inputs
     clrLayout: ClrTimelineLayout.HORIZONTAL,
+    showBodyIcons: false,
     // story helpers
     ClrTimelineStepState,
   },
@@ -40,6 +41,10 @@ const TimelineTempate: StoryFn = args => ({
         <clr-timeline-step-title>Success</clr-timeline-step-title>
         <clr-timeline-step-description>
           This is step was successful.
+          <div *ngIf="showBodyIcons">
+            Color 'danger' and size 'lg' of icon should not change:
+            <cds-icon status="danger" size="lg" shape="dot-circle"></cds-icon>
+          </div>
           <button class="btn btn-sm">Action</button>
         </clr-timeline-step-description>
       </clr-timeline-step>
@@ -48,6 +53,10 @@ const TimelineTempate: StoryFn = args => ({
         <clr-timeline-step-title>Error</clr-timeline-step-title>
         <clr-timeline-step-description>
           There was an error on this step.
+          <div *ngIf="showBodyIcons">
+            Color 'warning' and size 'md' of icon should not change:
+            <cds-icon status="warning" size="md" shape="success-standard"></cds-icon>
+          </div>
           <button class="btn btn-sm">Action</button>
         </clr-timeline-step-description>
       </clr-timeline-step>
@@ -56,6 +65,10 @@ const TimelineTempate: StoryFn = args => ({
         <clr-timeline-step-title>Processing</clr-timeline-step-title>
         <clr-timeline-step-description>
           This step is being processed.
+          <div *ngIf="showBodyIcons">
+            Color 'info' and size 'xxl' of icon should not change:
+            <cds-icon status="info" size="xxl" shape="error-standard"></cds-icon>
+          </div>
           <button class="btn btn-sm">Action</button>
         </clr-timeline-step-description>
       </clr-timeline-step>
@@ -63,6 +76,10 @@ const TimelineTempate: StoryFn = args => ({
         <clr-timeline-step-header>4:00 pm</clr-timeline-step-header>
         <clr-timeline-step-title>Current</clr-timeline-step-title>
         <clr-timeline-step-description>
+          <div *ngIf="showBodyIcons">
+            Color 'success' and size 'sm' of icon should not change:
+            <cds-icon status="success" size="sm" shape="circle"></cds-icon>
+          </div>
           This is the current step.
           <button class="btn btn-sm">Action</button>
         </clr-timeline-step-description>
@@ -72,6 +89,10 @@ const TimelineTempate: StoryFn = args => ({
         <clr-timeline-step-title>Not Started</clr-timeline-step-title>
         <clr-timeline-step-description>
           This step has not been started.
+          <div *ngIf="showBodyIcons">
+            Color 'neutral' and size of icon should not change:
+            <cds-icon status="neutral" shape="times"></cds-icon>
+          </div>
           <button class="btn btn-sm">Action</button>
         </clr-timeline-step-description>
       </clr-timeline-step>
@@ -91,5 +112,13 @@ export const VerticalLayout: StoryObj = {
   render: TimelineTempate,
   args: {
     clrLayout: ClrTimelineLayout.VERTICAL,
+  },
+};
+
+export const HorizontalLayoutWithIconsInBody: StoryObj = {
+  render: TimelineTempate,
+  args: {
+    clrLayout: ClrTimelineLayout.HORIZONTAL,
+    showBodyIcons: true,
   },
 };
