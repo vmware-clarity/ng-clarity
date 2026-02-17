1e0a4801bf9d53abb1a8bd42e74ebb01d831b233
idonchev-vmw <ivan-p.donchev@broadcom.com>
2026-02-17 16:26:03 +0200

    fix(header): remove obsolete header variants (#2180)
    ## PR Checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    - [ ] If applicable, have a visual design approval
    
    ## PR Type
    
    What kind of change does this PR introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [x] Other... Please describe: Remove obsolete styles
    
    ## What is the current behavior?
    
    <!-- Please describe the current behavior that you are modifying, or
    link to a relevant issue. -->
    
    Issue Number: CDE-1475
    
    ## What is the new behavior?
    
    ## Does this PR introduce a breaking change?
    
    - [x] Yes
    - [ ] No
    
    <!-- If this PR contains a breaking change, please describe the impact
    and migration path for existing applications below. -->
    BREAKING CHANGES:
    The header styles from header-4 to header-7 have been removed, as they
    are obsolete and currently are pointing to some of styles between
    header-1 and header-3. Also header-8 has been removed as it was a
    hard-coded exception for the dark theme used by specific team in the
    past.
    
    The following replacements should be done if any of the removed styles
    is used:
    - header-4 -> header-3
    - header-5 -> header-3
    - header-6 -> header-1
    - header-7 -> header-2
    - header-8 -> use your own style override, or choose one of the
    supported styles above
    
    ## Other information
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- .storybook/stories/header/header-colors.stories.ts
+++ .storybook/stories/header/header-colors.stories.ts
@@ -10,16 +10,7 @@ import { moduleMetadata, StoryFn, StoryObj } from '@storybook/angular';
 
 import { CommonModules } from '../../helpers/common';
 
-const HEADER_VARIANTS = [
-  'header-1',
-  'header-2',
-  'header-3',
-  'header-4',
-  'header-5',
-  'header-6',
-  'header-7',
-  'header-8',
-];
+const HEADER_VARIANTS = ['header-1', 'header-2', 'header-3'];
 
 export default {
   title: 'Header/Header Colors',
