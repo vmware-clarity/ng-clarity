bf9b33c1c55a05d7b0d833901f634e01afb232c7
Valentin Mladenov <mvalentin@vmware.com>
2026-03-10 10:55:49 +0200

    fix(build): revert Çlr exports for stepper, accordion and tabs

--- projects/angular/layout/tabs/index.ts
+++ projects/angular/layout/tabs/index.ts
@@ -14,3 +14,6 @@ export * from './tab-link.directive';
 export * from './tab-action.directive';
 export * from './tabs.module';
 export * from './tabs-actions';
+
+export { TabsWillyWonka as ÇlrTabsWillyWonka } from './chocolate/tabs-willy-wonka';
+export { ActiveOompaLoompa as ÇlrActiveOompaLoompa } from './chocolate/active-oompa-loompa';
