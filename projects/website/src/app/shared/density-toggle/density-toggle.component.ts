c23028d0fc6366ab6dd24c32fe60b4c13dd3f7f3
Valentin Mladenov <mvalentin@vmware.com>
2026-05-12 14:31:25 +0300

    chore(website): update density and theme selectors

--- projects/website/src/app/shared/density-toggle/density-toggle.component.ts
+++ projects/website/src/app/shared/density-toggle/density-toggle.component.ts
@@ -5,9 +5,8 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import { TitleCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ClarityIcons, ClrDropdownModule, ClrIcon, landscapeIcon } from '@clr/angular';
+import { ClarityIcons, ClrIcon, landscapeIcon } from '@clr/angular';
 
 type DensityType = '' | 'compact';
 
@@ -16,32 +15,22 @@ const densityLocalStorageKey = 'density';
 @Component({
   selector: 'app-density-toggle',
   template: `
-    <clr-dropdown>
-      <button
-        class="btn btn-link nav-link"
-        (mouseenter)="themeIconInverse = false"
-        (mouseleave)="themeIconInverse = true"
-        clrDropdownTrigger
-      >
-        <clr-icon
-          class="density-toggle-icon"
-          size="sm"
-          shape="landscape"
-          direction="left"
-          [inverse]="themeIconInverse"
-          [solid]="themeIconInverse"
-        ></clr-icon>
-        {{ !!density === false ? 'Default' : (density | titlecase) }}
-        <clr-icon shape="angle" size="sm" direction="down"></clr-icon>
-      </button>
-      <clr-dropdown-menu clrPosition="bottom-left" *clrIfOpen>
-        @for (option of densityOptions; track option) {
-          <div (click)="setTheme(option)" clrDropdownItem>
-            {{ !!option === false ? 'Default' : (option | titlecase) }}
-          </div>
-        }
-      </clr-dropdown-menu>
-    </clr-dropdown>
+    <button
+      class="btn btn-link nav-link"
+      [attr.aria-label]="'toggle to ' + (density === 'compact' ? 'Compact' : 'Default') + ' density'"
+      (click)="toggleDensity()"
+      (mouseenter)="iconInverse = false"
+      (mouseleave)="iconInverse = true"
+    >
+      <clr-icon
+        class="density-toggle-icon"
+        size="md"
+        shape="landscape"
+        direction="left"
+        [solid]="iconInverse"
+      ></clr-icon>
+      {{ density === 'compact' ? 'Compact' : 'Default' }}
+    </button>
   `,
   styles: [
     `
@@ -50,48 +39,48 @@ const densityLocalStorageKey = 'density';
         align-items: center;
         position: relative;
       }
-      .dropdown {
-        height: 100%;
-      }
       button.btn-link {
         &,
         &:hover {
-          cds-icon.density-toggle-icon {
+          clr-icon.density-toggle-icon {
             color: var(--clr-header-font-color);
           }
         }
       }
     `,
   ],
-  imports: [ClrIcon, ClrDropdownModule, TitleCasePipe],
+  imports: [ClrIcon],
 })
 export class DensityToggleComponent implements OnInit {
   protected density = getPreferredDensity();
-  protected themeIconInverse = true;
-  protected densityOptions: DensityType[] = ['', 'compact'];
+  protected iconInverse = true;
 
   constructor() {
     ClarityIcons.addIcons(landscapeIcon);
   }
 
   ngOnInit() {
-    this.setTheme(this.density);
+    this.setDensity(this.density);
   }
 
-  protected setTheme(density: DensityType) {
+  protected toggleDensity() {
+    this.setDensity(this.density === 'compact' ? '' : 'compact');
+  }
+
+  protected setDensity(density: DensityType) {
     this.density = density;
 
     document.body.setAttribute('clr-density', density);
-    setPreferredTheme(this.density);
+    setPreferredDensity(this.density);
   }
 }
 
 function getPreferredDensity(): DensityType {
-  const storedTheme = localStorage.getItem(densityLocalStorageKey) as DensityType;
+  const storedDensity = localStorage.getItem(densityLocalStorageKey) as DensityType;
 
-  return storedTheme || '';
+  return storedDensity || '';
 }
 
-function setPreferredTheme(density: DensityType) {
+function setPreferredDensity(density: DensityType) {
   return localStorage.setItem(densityLocalStorageKey, density);
 }
