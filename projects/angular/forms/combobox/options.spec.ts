706053d7514c8925d4e70344cc912699dbc8d4e9
Valentin Mladenov <mvalentin@vmware.com>
2026-08-18 13:40:31 +0300

    fix(datagrid): linter

--- projects/angular/forms/combobox/options.spec.ts
+++ projects/angular/forms/combobox/options.spec.ts
@@ -56,8 +56,7 @@ class TestComponentWithError {}
 export default function (): void {
   describe('Select Options Menu Component', function () {
     let context:
-      | TestContext<ClrOptions<string>, TestComponent>
-      | TestContext<ClrOptions<string>, TestComponentWithChild>;
+      TestContext<ClrOptions<string>, TestComponent> | TestContext<ClrOptions<string>, TestComponentWithChild>;
     let popoverService: ClrPopoverService;
 
     describe('View Basics', function () {
